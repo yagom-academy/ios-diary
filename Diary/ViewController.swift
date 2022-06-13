436a56203c85e3be134a9e942f2fe5ab03f3ea6f
Quokkaa <rktrkdml@gmail.com>
2022-06-13 22:55:54 +0900

    chore: SwiftLint 적용, 코드 컨벤션 수정 #1
    close #1

--- Diary/ViewController.swift
+++ Diary/ViewController.swift
@@ -1,18 +1,13 @@
 //
 //  Diary - ViewController.swift
-//  Created by yagom. 
+//  Created by Quokka Taeangel.
 //  Copyright © yagom. All rights reserved.
 // 
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
 }
-
