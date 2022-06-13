7084162baea7b1b3c9bb20cbc5755033d9434f0a
Eddy <kimkyunghun3@naver.com>
2022-06-13 21:47:53 +0900

    feat: json 파일 Decoding 기능 구현 #7

--- Diary/ViewController.swift
+++ Diary/ViewController.swift
@@ -11,3 +11,13 @@ class DiaryListViewController: UITableViewController {
         super.viewDidLoad()
     }
 }
+
+private extension Decodable {
+    static func parse(name: String) -> Self? {
+        guard let path = Bundle.main.path(forResource: name, ofType: "json") else { return nil }
+        guard let jsonString = try? String(contentsOfFile: path) else { return nil }
+        guard let data = jsonString.data(using: .utf8) else { return nil }
+
+        return try? JSONDecoder().decode(Self.self, from: data)
+    }
+}
