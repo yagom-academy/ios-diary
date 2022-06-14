294b770accafc29da8927a0eafcf64d05404b94d
Lingo <llingo@kakao.com>
2022-06-14 20:50:21 +0900

    feat: DiaryDetailViewController의 configure 메서드 구현

--- Diary/Controller/DiaryDetailViewController.swift
+++ Diary/Controller/DiaryDetailViewController.swift
@@ -6,6 +6,8 @@
 import UIKit
 
 final class DiaryDetailViewController: UIViewController {
+  private let titleLabel = UILabel()
+  private let bodyText = UITextView()
   private let diary: Diary
 
   init(diary: Diary) {
@@ -19,6 +21,40 @@ final class DiaryDetailViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.configure()
+  }
+
+  private func configure() {
+    self.title = formatNumberToDateString(from: diary.createdAt)
     self.view.backgroundColor = .systemBackground
+
+    self.titleLabel.font = .preferredFont(forTextStyle: .headline)
+    self.titleLabel.adjustsFontForContentSizeCategory = true
+    self.titleLabel.text = diary.title
+
+    self.bodyText.font = .preferredFont(forTextStyle: .body)
+    self.bodyText.adjustsFontForContentSizeCategory = true
+    self.bodyText.text = diary.body
+
+    let container = UIStackView(arrangedSubviews: [titleLabel, bodyText])
+    container.axis = .vertical
+    container.spacing = 20.0
+
+    self.view.addSubview(container)
+    container.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      container.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8.0),
+      container.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      container.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8.0),
+      container.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8.0)
+    ])
+  }
+
+  private func formatNumberToDateString(from dateNumber: Int) -> String {
+    let date = Date(timeIntervalSince1970: Double(dateNumber))
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+
+    return dateFormatter.string(from: date)
   }
 }
