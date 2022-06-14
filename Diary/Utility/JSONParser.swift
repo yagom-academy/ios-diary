92b5ed41bcf6fe011324a30aa902d4d94c71f696
Mino777 <jomino7777@gmail.com>
2022-06-14 15:31:14 +0900

    refactor: JSONParser decode 메서드 수정

--- Diary/Utility/JSONParser.swift
+++ Diary/Utility/JSONParser.swift
@@ -10,12 +10,12 @@ import UIKit
 struct JSONParser {
     private let decoder = JSONDecoder()
     
-    func decode<T: Decodable>(from fileName: String) -> T? {
+    func decode<T: Decodable>(from fileName: String) -> [T]? {
         guard let assetData = NSDataAsset(name: fileName) else {
             return nil
         }
         
-        let decodedData = try? decoder.decode(T.self, from: assetData.data)
+        let decodedData = try? decoder.decode([T].self, from: assetData.data)
         return decodedData
     }
 }
