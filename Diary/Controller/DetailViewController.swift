015e7afe877c89e311fbcd9698131ebb567a1471
JoSH0318 <jsh9870@naver.com>
2022-06-14 16:28:09 +0900

    feat: DetailVC setUpNavigationBar 메서드 구현

--- Diary/Controller/DetailViewController.swift
+++ Diary/Controller/DetailViewController.swift
@@ -19,4 +19,7 @@ final class DetailViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    private func setUpNavigationBar() {
+        title = diary.createdAt.formattedString
+    }
 }
