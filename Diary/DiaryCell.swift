cb74a15167e660bd0bfa9439dbf78ce9907e5d31
Tiana <97kth01@naver.com>
2022-06-14 12:20:28 +0900

    feat: tableView 구현

--- Diary/DiaryCell.swift
+++ Diary/DiaryCell.swift
@@ -7,38 +7,46 @@
 
 import UIKit
 
-final class DiaryCell: UITableViewCell {
+extension UITableViewCell {
     static var identifier: String {
         return String(describing: Self.self)
     }
-    
+}
+
+final class DiaryCell: UITableViewCell {
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.font = .preferredFont(forTextStyle: .title3)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let dateLabel: UILabel = {
         let label = UILabel()
         label.font = .preferredFont(forTextStyle: .body)
+        label.setContentCompressionResistancePriority(.required, for: .horizontal)
         return label
     }()
     
     private let bodyLabel: UILabel = {
         let label = UILabel()
-        label.font = .preferredFont(forTextStyle: .callout)
+        label.font = .preferredFont(forTextStyle: .caption1)
         return label
     }()
     
     private let informationStackView: UIStackView = {
         let stackView = UIStackView()
-        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 10
         return stackView
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        informationStackView.addArrangedSubviews(dateLabel, bodyLabel)
+        
+        accessoryType = .disclosureIndicator
+        
+        addSubviews()
         setUpLayout()
     }
     
@@ -46,25 +54,34 @@ final class DiaryCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configure(data: SampleData) {
+    func configure(data: DiaryData) {
         titleLabel.text = data.title
-        dateLabel.text = data.date.description
+        dateLabel.text = data.date
         bodyLabel.text = data.body
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(informationStackView)
+        
+        informationStackView.addArrangedSubviews(dateLabel, bodyLabel)
     }
     
     private func setUpLayout() {
         func setUpTitleLayout() {
             NSLayoutConstraint.activate([
-                titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
-                titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
+                titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+                titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+                titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20)
             ])
         }
         
         func setupInfoLayout() {
             NSLayoutConstraint.activate([
-                informationStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-                informationStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-                informationStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+                informationStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+                informationStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+                informationStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+                informationStackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10)
             ])
         }
         
--- Diary/DiaryViewController.swift
+++ Diary/DiaryViewController.swift
@@ -7,12 +7,78 @@
 import UIKit
 
 final class DiaryViewController: UIViewController {
-
+    enum Section {
+        case main
+    }
+    
+    let tableView = UITableView()
+    var dataSource: UITableViewDiffableDataSource<Section, DiaryData>?
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        setUpDataSource()
+        setUpTableView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         setUpNavigationController()
+        
+        setUpTableViewLayout()
+        
+        guard let diaryData = AssetManager().getData() else {
+            return
+        }
+        
+        setUpSanpshot(data: diaryData)
+    }
+    
+    private func setUpTableView() {
+        tableView.register(DiaryCell.self, forCellReuseIdentifier: DiaryCell.identifier)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.identifier)
+        tableView.dataSource = dataSource
+    }
+    
+    private func setUpTableViewLayout() {
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    private func setUpDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section, DiaryData>(tableView: tableView) {
+            tableView, indexPath, itemIdentifier in
+            
+            if let cell = tableView.dequeueReusableCell(withIdentifier: DiaryCell.identifier, for: indexPath) as? DiaryCell {
+                cell.configure(data: itemIdentifier)
+                return cell
+            }
+            
+            return tableView.dequeueReusableCell(withIdentifier: UITableViewCell.identifier, for: indexPath)
+        }
+    }
+    
+    private func setUpSanpshot(data: [DiaryData]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, DiaryData>()
+        
+        snapshot.appendSections([.main])
+        snapshot.appendItems(data)
+        
+        dataSource?.apply(snapshot)
     }
     
     private func setUpNavigationController() {
