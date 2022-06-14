de413fc44090fd6a05545f604a3f44330183ebf5
Mino777 <jomino7777@gmail.com>
2022-06-14 11:57:35 +0900

    feat: MainView 생성 및 이니셜라이저 선언

---
+++ Diary/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Diary
+//
+//  Created by mmim, grumpy, mino on 2022/06/14.
+//
+
+import UIKit
+
+final class MainView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
