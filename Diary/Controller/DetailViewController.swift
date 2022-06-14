872c2cdb34c7e3055f11adf09ab6ad954245cc50
JoSH0318 <jsh9870@naver.com>
2022-06-14 16:27:44 +0900

    feat: DetailVC init 구현

--- Diary/Controller/DetailViewController.swift
+++ Diary/Controller/DetailViewController.swift
@@ -10,4 +10,13 @@ import UIKit
 final class DetailViewController: UIViewController {
     private lazy var detailView = DetailView(frame: view.bounds)
     private let diary: Diary
+    
+    init(diary: Diary) {
+        self.diary = diary
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
