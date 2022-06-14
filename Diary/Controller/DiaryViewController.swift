85314536e8b372b0dfb6445c9d85653b674110e6
minseong <msb0987@naver.com>
2022-06-14 15:42:59 +0900

    refactor: ViewController -> DiaryViewController 변경

--- Diary/Controller/DiaryViewController.swift
+++ Diary/Controller/DiaryViewController.swift
@@ -1,11 +1,11 @@
 //
-//  Diary - ViewController.swift
+//  Diary - DiaryViewController.swift
 //  Created by Minseong, Lingo
 //
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class DiaryViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
