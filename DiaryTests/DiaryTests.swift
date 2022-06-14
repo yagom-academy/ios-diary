bad8a2e9927556a6dc90d5672936185e19190e2d
Lingo <llingo@kakao.com>
2022-06-14 15:13:38 +0900

    test: DiaryTests 타겟 추가

---
+++ DiaryTests/DiaryTests.swift
@@ -0,0 +1,19 @@
+//
+//  DiaryTests.swift
+//  DiaryTests
+//
+//  Created by Minseong, Lingo on 2022/06/14.
+//
+
+import XCTest
+@testable import Diary
+
+final class DiaryTests: XCTestCase {
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+  }
+
+  override func tearDownWithError() throws {
+    try super.tearDownWithError()
+  }
+}
