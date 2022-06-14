cc8727b4c1b14a80fe99105944bfe95cec7805a6
dudu <whqtkf12@naver.com>
2022-06-14 10:43:36 +0900

    feat: Diary 모델 생성 및 decoding test 추가

---
+++ Diary/Model/Diary.swift
@@ -0,0 +1,30 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by dudu, papri on 2022/06/14.
+//
+
+import Foundation
+
+struct Diary: Decodable {
+    let title: String
+    let body: String
+    let createdDate: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case body
+        case createdDate = "created_at"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        title = try values.decode(String.self, forKey: .title)
+        body = try values.decode(String.self, forKey: .body)
+        
+        let interval = try values.decode(Double.self, forKey: .createdDate)
+        
+        createdDate = Date(timeIntervalSince1970: interval)
+    }
+}
