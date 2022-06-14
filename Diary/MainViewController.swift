a6f78b36e7988cfd14831577cea1415dd8c4d0ff
Mino777 <jomino7777@gmail.com>
2022-06-14 10:15:27 +0900

    refactor: 불필요한 코드 제거

--- Diary/MainViewController.swift
+++ Diary/MainViewController.swift
@@ -1,18 +1,15 @@
 //
-//  Diary - ViewController.swift
+//  Diary - MainViewController.swift
 //  Created by yagom. 
 //  Copyright © yagom. All rights reserved.
 // 
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
