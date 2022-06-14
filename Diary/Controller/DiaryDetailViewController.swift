e2d8e43e63fbc405f1e4506017a0097387b7338b
minseong <msb0987@naver.com>
2022-06-14 20:22:31 +0900

    feat: DiaryDetailViewController 구현

--- Diary/Controller/DiaryDetailViewController.swift
+++ Diary/Controller/DiaryDetailViewController.swift
@@ -3,4 +3,22 @@
 //  Created by Minseong, Lingo
 //
 
-import Foundation
+import UIKit
+
+final class DiaryDetailViewController: UIViewController {
+  private let diary: Diary
+
+  init(diary: Diary) {
+    self.diary = diary
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .systemBackground
+  }
+}
