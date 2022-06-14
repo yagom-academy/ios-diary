829fe55d34b727a310b782046c7d473ef3dcc985
Red <seryprk@gmail.com>
2022-06-14 15:37:35 +0900

    feat : DiaryData, DiaryDateFormatter 모델 생성

---
+++ Diary/Model/DiaryData.swift
@@ -0,0 +1,14 @@
+//
+//  DiaryData.swift
+//  Diary
+//
+//  Created by 우롱차, RED on 2022/06/14.
+//
+
+import Foundation
+
+struct DiaryData {
+    private(set) var title: String
+    private(set) var date: Date
+    private(set) var body: String
+}
