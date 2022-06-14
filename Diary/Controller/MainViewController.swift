e2297dad5de2a73e7e3036e2bd7415ac3c1e0e89
Mino777 <jomino7777@gmail.com>
2022-06-14 12:04:13 +0900

    feat: MainVC loadView에서 mainView로 변경

--- Diary/Controller/MainViewController.swift
+++ Diary/Controller/MainViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 final class MainViewController: UIViewController {
     private lazy var mainView = MainView(frame: view.bounds)
 
+    override func loadView() {
+        super.loadView()
+        view = mainView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
