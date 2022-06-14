0d5053d7b9ef1b652924449584db4c9b891c7eab
Grumpy <sewoong2890@gmail.com>
2022-06-14 14:56:38 +0900

    feat: 샘플 데이터를 decode하여 배열에 저장하는 기능 생성

--- Diary/Controller/MainViewController.swift
+++ Diary/Controller/MainViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 final class MainViewController: UIViewController {
     private lazy var mainView = MainView(frame: view.bounds)
 
+    private let parser = JSONDecoder()
+    private var diary: [Diary]?
+    
     override func loadView() {
         super.loadView()
         view = mainView
@@ -16,5 +19,9 @@ final class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainView.baseTableView.dataSource = self
+        diary = try? parser.decode([Diary].self, from: NSDataAsset(name: "sample")!.data)
+    }
+}
     }
 }
