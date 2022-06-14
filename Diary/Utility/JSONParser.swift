92fdbc083a1023d0c8ce3dde78433841468317b8
Mino777 <jomino7777@gmail.com>
2022-06-14 15:22:05 +0900

    feat: JSONParser 구현

--- Diary/Utility/JSONParser.swift
+++ Diary/Utility/JSONParser.swift
@@ -5,8 +5,17 @@
 //  Created by 조민호 on 2022/06/14.
 //
 
-import Foundation
+import UIKit
 
 struct JSONParser {
+    private let decoder = JSONDecoder()
     
+    func decode<T: Decodable>(from fileName: String) -> T? {
+        guard let assetData = NSDataAsset(name: fileName) else {
+            return nil
+        }
+        
+        let decodedData = try? decoder.decode(T.self, from: assetData.data)
+        return decodedData
+    }
 }
