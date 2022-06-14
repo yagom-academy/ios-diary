9dac4014e8999c2e1dc97a68a4ae9419b78b7c78
marisol <marisol.develop@gmail.com>
2022-06-14 15:36:47 +0900

    💎feat : Json 디코딩을 위한 Diary 모델 생성

---
+++ Diary/DiaryData.swift
@@ -0,0 +1,25 @@
+//
+//  DiaryData.swift
+//  Diary
+//
+//  Created by 두기, marisol on 2022/06/14.
+//
+
+import Foundation
+
+struct DiaryData: Decodable {
+    let title: String?
+    let body: String?
+    let createdAt: Int?
+    
+    private enum CodingKeys: String, CodingKey {
+        case title, body
+        case createdAt = "created_at"
+    }
+}
+
+struct Diary {
+    let title: String
+    let body: String
+    let createdAt: String
+}
