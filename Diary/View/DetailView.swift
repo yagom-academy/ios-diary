9cd463798039d5aad366c11826ebcc43b1c31eda
JoSH0318 <jsh9870@naver.com>
2022-06-14 16:21:03 +0900

    feat: DetailView 생성 및 init 구현

--- Diary/View/DetailView.swift
+++ Diary/View/DetailView.swift
@@ -5,4 +5,16 @@
 //  Created by 조성훈 on 2022/06/14.
 //
 
-import Foundation
+import UIKit
+
+final class DetailView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpView()
+    }
+}
