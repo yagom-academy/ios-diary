3cee8ab00c5085ab473c965c57085db4682046d4
malrang <ruehd7924@gmail.com>
2022-06-14 22:08:29 +0900

    feat: KeyboardViewController 추상 클래스 구현

---
+++ Diary/Controller/KeyboardViewController.swift
@@ -0,0 +1,27 @@
+//
+//  KeyboardViewController.swift
+//  Diary
+//
+//  Created by 김동욱 on 2022/06/14.
+//
+
+import UIKit
+
+class KeyboardViewController: UIViewController {
+    var bottomContraint: NSLayoutConstraint?
+    
+    func setUpKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillAppear(_ notification: Notification) {
+        guard let keyboardBounds = notification.userInfo?["UIKeyboardBoundsUserInfoKey"] as? NSValue else { return }
+        
+        bottomContraint?.constant = -keyboardBounds.cgRectValue.height
+    }
+    
+    @objc private func keyboardWillHide() {
+        bottomContraint?.constant = .zero
+    }
+}
--- Diary/Controller/RegisterViewController.swift
+++ Diary/Controller/RegisterViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-final class RegisterViewController: UIViewController {
-    enum Constant {
-        static let backButton = "일기장"
+final class RegisterViewController: KeyboardViewController {
+    private let textView = UITextView()
+    private var backButtonTitle: String?
+    
+    init(backButtonTitle: String?) {
+        super.init(nibName: nil, bundle: nil)
+        self.backButtonTitle = backButtonTitle
     }
     
-    private let textView = UITextView()
-    private var bottonContraint: NSLayoutConstraint?
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         view.backgroundColor = .systemBackground
@@ -24,13 +29,9 @@ final class RegisterViewController: UIViewController {
     
     private func setUpNavigationController() {
         func setUpLeftItem() {
-            let button = UIButton()
+            let button = UIButton(title: backButtonTitle)
             let barButton = UIBarButtonItem(customView: button)
             
-            button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
-            button.setTitle("일기장", for: .normal)
-            button.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
-            button.setTitleColor(.systemBlue, for: .normal)
             button.addTarget(self, action: #selector(registerDiary), for: .touchUpInside)
             
             navigationItem.leftBarButtonItem = barButton
@@ -48,29 +49,14 @@ final class RegisterViewController: UIViewController {
         view.addSubview(textView)
         
         textView.translatesAutoresizingMaskIntoConstraints = false
-        
-        bottonContraint = textView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+       
+        bottomContraint = textView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         
         NSLayoutConstraint.activate([
             textView.topAnchor.constraint(equalTo: view.topAnchor),
             textView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             textView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            bottonContraint
+            bottomContraint
         ].compactMap { $0 })
-    }
-    
-    private func setUpKeyboardNotification() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc private func keyboardWillAppear(_ notification: Notification) {
-        guard let keyboardBounds = notification.userInfo?["UIKeyboardBoundsUserInfoKey"] as? NSValue else { return }
-
-        bottonContraint?.constant = -keyboardBounds.cgRectValue.height
-    }
-    
-    @objc private func keyboardWillHide() {
-        bottonContraint?.constant = .zero
     }
 }
