9b380c6610117d3c7d12c5d5bb42e74942cc097a
kimt4580 <xogns8696@gmail.com>
2022-06-15 17:35:10 +0900

    ✨Feat : DiaryModel 생성 및 구현

---
+++ Diary/Model/DiaryModel.swift
@@ -0,0 +1,19 @@
+//
+//  DiaryModel.swift
+//  Diary
+//
+//  Created by Donnie, OneTool on 2022/06/15.
+//
+
+import Foundation
+
+struct DiaryModel {
+    let title: String?
+    let body: String?
+    let createdAt: TimeInterval?
+    
+    private enum CodinKeys: String, CodingKey {
+        case createdAt = "created_at"
+        case title, body
+    }
+}
