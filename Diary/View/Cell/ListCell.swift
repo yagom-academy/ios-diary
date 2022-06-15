52712eb9c4ab42b62b0bf08d0542ca29c20afa56
kimt4580 <xogns8696@gmail.com>
2022-06-15 17:36:34 +0900

    ✨Feat : ListCell 생성 및 구현

---
+++ Diary/View/Cell/ListCell.swift
@@ -0,0 +1,61 @@
+//
+//  ListCell.swift
+//  Diary
+//
+//  Created by Donnie, OneTool on 2022/06/15.
+//
+
+import UIKit
+
+final class ListCell: UICollectionViewCell, CellMakeable {
+    var titleLabel: UILabel = UILabel()
+    var dateLabel: UILabel = UILabel()
+    var descriptionLabel: UILabel = UILabel()
+    var verticalStackView: UIStackView = UIStackView()
+    var horizontalStackView: UIStackView = UIStackView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        titleLabel = createLabel(font: .preferredFont(forTextStyle: .body),
+                                 textColor: .black,
+                                 alignment: .left)
+        dateLabel = createLabel(font: .preferredFont(forTextStyle: .body),
+                                textColor: .black,
+                                alignment: .left)
+        descriptionLabel = createLabel(font: .preferredFont(forTextStyle: .body),
+                                       textColor: .black,
+                                       alignment: .left)
+        verticalStackView = createStackView(axis: .vertical,
+                                            alignment: .leading,
+                                            distribution: .fillEqually,
+                                            spacing: 5)
+        horizontalStackView = createStackView(axis: .horizontal,
+                                              alignment: .leading,
+                                              distribution: .fillEqually,
+                                              spacing: 5)
+        setConstraints()
+        setSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setSubviews() {
+        horizontalStackView.addArrangedSubview(dateLabel)
+        horizontalStackView.addArrangedSubview(descriptionLabel)
+        verticalStackView.addArrangedSubview(titleLabel)
+        verticalStackView.addArrangedSubview(horizontalStackView)
+        contentView.addSubview(verticalStackView)
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            verticalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            verticalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            verticalStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            verticalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
