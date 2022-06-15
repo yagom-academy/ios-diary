ceb1d3658b781c6a9e11324d4b30b0acd73cfb07
kimt4580 <xogns8696@gmail.com>
2022-06-15 17:33:35 +0900

    ✨Feat : MainView 생성

---
+++ Diary/View/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  MainView.swift
+//  Diary
+//
+//  Created by Donnie, OneTool on 2022/06/15.
+//
+
+import UIKit
+
+final class MainView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
