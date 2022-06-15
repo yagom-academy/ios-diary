616516c0d266462cab0673882c10d41cee7b0bb3
Quokkaa <rktrkdml@gmail.com>
2022-06-15 14:56:59 +0900

    file: 그룹 폴더 생성 및 DiaryDetailViewController 파일 생성

---
+++ DetailDriary/Controller/DetailDiaryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailDiaryViewController.swift
+//  Diary
+//
+//  Created by Taeangel, Quokka on 2022/06/15.
+//
+
+import UIKit
+
+class DetailDiaryViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
