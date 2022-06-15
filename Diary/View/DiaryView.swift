9bac32c490f7c21ed18379aeef51dd9cb565d565
doogie97 <chris_97@naver.com>
2022-06-15 12:41:29 +0900

    💎feat : textView수정시 키보드가 textView를 가리지 않게 하는 기능 구현

--- Diary/View/DiaryView.swift
+++ Diary/View/DiaryView.swift
@@ -17,6 +17,10 @@ final class DiaryView: UIView {
         super.init(coder: coder)
     }
     
+    deinit {
+        removeKeyboardObserver()
+    }
+    
     private lazy var diaryTextView: UITextView = {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
@@ -41,5 +45,36 @@ final class DiaryView: UIView {
         \(diary.body)
         """
         diaryTextView.contentOffset = CGPoint(x: 0, y: 0)
+        addKeyboardObserver()
+    }
+}
+
+// MARK: - about keyboard
+extension DiaryView {
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+        
+        let contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.height, right: 0.0)
+        diaryTextView.contentInset = contentInset
+        diaryTextView.scrollIndicatorInsets = contentInset
+    }
+    
+    @objc private func keyboardWillHide() {
+        let contentInset = UIEdgeInsets.zero
+        diaryTextView.contentInset = contentInset
+        diaryTextView.scrollIndicatorInsets = contentInset
+    }
+    
+    private func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func removeKeyboardObserver() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
