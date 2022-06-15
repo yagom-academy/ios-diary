ce640273f1a2a23c7b809ada662e83e64d886b6e
Donnie <seo_17@naver.com>
2022-06-15 21:52:52 +0900

    ✨Feat : mainView NavigationBarTitle 설정

--- Diary/ViewController/MainViewController.swift
+++ Diary/ViewController/MainViewController.swift
@@ -7,11 +7,21 @@
 import UIKit
 
 final class MainViewController: UIViewController {
+    
+    private enum Constants {
+        static let navigationBarTitle = "일기장"
+    }
+    
     private lazy var mainView = MainView(frame: view.frame)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view = mainView
+        setNavigationTitle()
+    }
+    
+    private func setNavigationTitle() {
+        navigationController?.navigationBar.topItem?.title = Constants.navigationBarTitle
     }
 }
 
