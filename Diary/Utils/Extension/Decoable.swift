06b52eae9f8ca04a38beb85ab84b82684eee483f
Quokkaa <rktrkdml@gmail.com>
2022-06-15 11:16:41 +0900

    feat: passing 메서드 구현

--- Diary/Utils/Extension/Decoable.swift
+++ Diary/Utils/Extension/Decoable.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension Decodable {
+  static func parse(data: Data) -> Self? {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    
+    guard let diary = try? decoder.decode(Self.self, from: data) else {
+      return nil
+    }
+    
+    return diary
+  }
+}
