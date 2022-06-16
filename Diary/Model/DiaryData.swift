2afeb781162e993dc6d407802b264ad206983c43
Taeangel <wngks2050@gmail.com>
2022-06-16 16:40:22 +0900

    refactor: DiaryData 파일분리

---
+++ Diary/Model/DiaryData.swift
@@ -0,0 +1,24 @@
+//
+//  DiaryData.swift
+//  Diary
+//
+//  Created by Taeangel, Quokka on 2022/06/16.
+//
+
+import Foundation
+
+struct DiaryData {
+  static func saveJsonData() -> [Diary]? {
+    guard let path = Bundle.main.path(forResource: "sample", ofType: "json") else {
+      return nil
+    }
+    
+    guard let jsonData = try? String(contentsOfFile: path).data(using: .utf8) else {
+      return nil
+    }
+
+    let diaryData = try? Json.decoder.decode([Diary].self, from: jsonData)
+    
+    return diaryData
+  }
+}
