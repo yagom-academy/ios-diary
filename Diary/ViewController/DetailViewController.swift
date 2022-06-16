cc1daec5e9364475bbab38f103191b8ad5a3f12f
kimt4580 <xogns8696@gmail.com>
2022-06-17 06:41:53 +0900

    ✨Feat : keyboard 구현 및 전달 받은 데이터 입력

--- Diary/ViewController/DetailViewController.swift
+++ Diary/ViewController/DetailViewController.swift
@@ -9,9 +9,54 @@ import UIKit
 
 final class DetailViewController: UIViewController {
     private lazy var detailView = DetailView(frame: view.frame)
+    private var diaryData: DiaryModel
+    
+    init(diaryData: DiaryModel) {
+        self.diaryData = diaryData
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view = detailView
+        setNavigationBarTitle()
+        detailView.titleField.text = diaryData.title
+        detailView.descriptionView.text = diaryData.body
+    }
+    
+    private func setNavigationBarTitle() {
+        navigationItem.title = diaryData.createdAt?.formattedDate
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification , object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification , object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            self.detailView.mainScrollView.contentSize = .init(width: self.detailView.mainScrollView.frame.width, height: self.detailView.mainScrollView.frame.height + keyboardSize.height - 140)
+            
+            if self.detailView.descriptionView.isFirstResponder {
+                detailView.mainScrollView.scrollRectToVisible(detailView.descriptionView.frame, animated: true)
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            self.detailView.mainScrollView.contentSize = .init(width: self.detailView.mainScrollView.frame.width, height: self.detailView.mainScrollView.frame.height - keyboardSize.height + 140)
+        }
     }
 }
