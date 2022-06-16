af6b9497ca42ac2a949fda75b379bf2582f793cc
kimt4580 <xogns8696@gmail.com>
2022-06-17 06:38:06 +0900

    ✨Feat : DetailView 구현

--- Diary/View/DetailView.swift
+++ Diary/View/DetailView.swift
@@ -8,5 +8,75 @@
 import UIKit
 
 final class DetailView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        setSubviews()
+        setConstraints()
+    }
     
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 5
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        return stackView
+    }()
+    
+    let mainScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isScrollEnabled = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0)
+        
+        return scrollView
+    }()
+    
+    let titleField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.textAlignment = .left
+        return textField
+    }()
+    
+    let descriptionView: UITextView = {
+        let textView = UITextView()
+        textView.font = .preferredFont(forTextStyle: .body)
+        textView.textAlignment = .left
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    private func setSubviews() {
+        mainStackView.addArrangedSubview(titleField)
+        mainStackView.addArrangedSubview(descriptionView)
+        mainScrollView.addSubview(mainStackView)
+        self.addSubview(mainScrollView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            mainScrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            mainScrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15),
+            mainScrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15),
+            mainScrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            mainScrollView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            mainScrollView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            mainStackView.topAnchor.constraint(equalTo: mainScrollView.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: mainScrollView.trailingAnchor),
+            mainStackView.centerXAnchor.constraint(equalTo: mainScrollView.centerXAnchor),
+            mainStackView.centerYAnchor.constraint(equalTo: mainScrollView.centerYAnchor)
+            ])
+    }
 }
