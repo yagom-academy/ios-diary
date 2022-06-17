fa01398d78b5118444c8e7e9b2250fb3e633db0b
Grumpy <sewoong2890@gmail.com>
2022-06-17 11:26:21 +0900

    feat: RegisterVC 생성

---
+++ Diary/Controller/RegistrationViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RegistrationViewController.swift
+//  Diary
+//
+//  Created by mmim, grumpy, mino on 2022/06/17.
+//
+
+import UIKit
+
+final class RegistrationViewController: UIViewController {
+}
+
