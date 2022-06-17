f195dd006b641d219a00b67001cf26c370e7502b
Lingo <llingo@kakao.com>
2022-06-17 17:05:05 +0900

    feat: 열거형 Parser 타입 생성 및 decode 메서드 구현

--- Diary/Util/Parser.swift
+++ Diary/Util/Parser.swift
@@ -5,4 +5,17 @@
 //  Created by Minseong, Lingo on 2022/06/17.
 //
 
-import Foundation
+import UIKit
+
+enum Parser {
+  private static let jsonDecoder = JSONDecoder()
+
+  static func decode<T: Decodable>(type: T.Type, assetName: String) -> T? {
+    jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+
+    guard let dataAsset = NSDataAsset(name: assetName) else { return nil }
+    guard let result = try? jsonDecoder.decode(type, from: dataAsset.data) else { return nil }
+
+    return result
+  }
+}
