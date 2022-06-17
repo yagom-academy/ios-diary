e2baf2f0b981415f71c7e70d1713bfa14e49fea7
Grumpy <sewoong2890@gmail.com>
2022-06-17 20:46:38 +0900

    feat: ShareActivityItemSource 클래스 구현

--- Diary/View/ShareActivityItemSource.swift
+++ Diary/View/ShareActivityItemSource.swift
@@ -2,7 +2,46 @@
 //  ShareActivityItemSource.swift
 //  Diary
 //
-//  Created by 박세웅 on 2022/06/17.
+//  Created by mmim, grumpy, mino on 2022/06/17.
 //
 
-import Foundation
+import LinkPresentation
+
+final class ShareActivityItemSource: NSObject, UIActivityItemSource {
+    var title: String
+    var text: String
+    
+    init(title: String, text: String) {
+        self.title = title
+        self.text = text
+    }
+    
+    func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+        return text
+    }
+    
+    func activityViewController(
+        _ activityViewController: UIActivityViewController,
+        itemForActivityType activityType: UIActivity.ActivityType?
+    ) -> Any? {
+        return text
+    }
+    
+    func activityViewController(
+        _ activityViewController: UIActivityViewController,
+        subjectForActivityType activityType: UIActivity.ActivityType?
+    ) -> String {
+        return title
+    }
+    
+    func activityViewControllerLinkMetadata(_ activityViewController: UIActivityViewController) -> LPLinkMetadata? {
+        guard let icon = UIImage(systemName: "person.circle") else {
+            return nil
+        }
+        let metadata = LPLinkMetadata()
+        metadata.title = title
+        metadata.iconProvider = NSItemProvider(object: icon)
+        metadata.originalURL = URL(fileURLWithPath: text)
+        return metadata
+    }
+}
