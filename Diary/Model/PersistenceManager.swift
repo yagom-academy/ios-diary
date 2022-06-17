b3976b55897c906e2f5ad28da2d8d62ea81d62a8
Grumpy <sewoong2890@gmail.com>
2022-06-17 11:03:31 +0900

    feat: PersistenceManager 클래스 구현

--- Diary/Model/PersistenceManager.swift
+++ Diary/Model/PersistenceManager.swift
@@ -2,7 +2,73 @@
 //  PersistenceManager.swift
 //  Diary
 //
-//  Created by 박세웅 on 2022/06/17.
+//  Created by mmim, grumpy, mino on 2022/06/17.
 //
 
 import Foundation
+import CoreData
+
+final class PersistenceManager {
+    static let shared = PersistenceManager()
+    
+    private init() { }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "DiaryEntity")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError( )
+            }
+        })
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return self.persistentContainer.viewContext
+    }
+    
+    func fetchData(request: NSFetchRequest<DiaryEntity>) -> [DiaryEntity] {
+        do {
+            let fetchResult = try self.context.fetch(request)
+            return fetchResult
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+    
+    func createData(data: Diary) {
+        let entity = NSEntityDescription.entity(forEntityName: "Joke", in: self.context)
+        if let entity = entity {
+            let diary = NSManagedObject(entity: entity, insertInto: self.context)
+            diary.setValue(data.title, forKey: "title")
+            diary.setValue(data.body, forKey: "body")
+            diary.setValue(data.createdAt, forKey: "createdAt")
+            
+            do {
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func deleteData(object: NSManagedObject) {
+        self.context.delete(object)
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func modifyData() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
