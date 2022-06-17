887a3a8eadbe0c5f1a33ac1b41ec329a1dc6309f
Grumpy <sewoong2890@gmail.com>
2022-06-17 11:26:54 +0900

    feat: Date에 extension으로 formattedString 구현

--- Diary/Controller/RegistrationViewController.swift
+++ Diary/Controller/RegistrationViewController.swift
@@ -10,3 +10,11 @@ import UIKit
 final class RegistrationViewController: UIViewController {
 }
 
+fileprivate extension Date {
+    var formattedString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.locale = Locale(identifier: Locale.preferredLanguages.first ?? "ko_KR")
+        return dateFormatter.string(from: self)
+    }
+}
