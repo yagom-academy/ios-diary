3c6b5e6a1fc93c36c7f57b9db307db36d06eda35
minseong <msb0987@naver.com>
2022-06-17 16:07:55 +0900

    feat: Then Protocol 구현

--- Diary/Util/Then.swift
+++ Diary/Util/Then.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+protocol Then {}
+
+extension Then where Self: AnyObject {
+  func then(_ block: (Self) throws -> Void) rethrows -> Self {
+    try block(self)
+
+    return self
+  }
+}
+
+extension NSObject: Then {}
