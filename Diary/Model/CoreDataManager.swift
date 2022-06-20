ebe104eff1977d207c40d08254895d78ecd5cce4
marisol <marisol.develop@gmail.com>
2022-06-20 11:45:42 +0900

    💎feat : CoreDataManager 생성 및 create 메서드 구현

---
+++ Diary/Model/CoreDataManager.swift
@@ -0,0 +1,56 @@
+//
+//  CoreDataManager.swift
+//  Diary
+//
+//  Created by 두기, marisol on 2022/06/20.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    static let shared = CoreDataManager()
+    private init() {}
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Diary")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    var entity: NSEntityDescription? {
+        return NSEntityDescription.entity(forEntityName: "DiaryModel", in: context)
+    }
+    
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    func create(_ diaryData: DiaryData) {
+        guard let entity = entity else {
+            return
+        }
+        
+        let diary = NSManagedObject(entity: entity, insertInto: context)
+        diary.setValue(diaryData.title, forKey: "title")
+        diary.setValue(diaryData.body, forKey: "body")
+        diary.setValue(diaryData.createdAt, forKey: "createdAt")
+        
+        saveContext()
+    }
+}
