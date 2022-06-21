d13221af38aafdef45b132a30723acaa139d4d98
Taeangel <wngks2050@gmail.com>
2022-06-21 18:37:30 +0900

    feat: CoredataManager Singleton에 createContext 메서드 구현 #8

--- Diary/Utils/Sington/CoreDataManager.swift
+++ Diary/Utils/Sington/CoreDataManager.swift
@@ -14,7 +14,7 @@ final class CoredataManager {
   private init() {}
   
   private lazy var persistantContainer: NSPersistentContainer = {
-    let persistantContainer = NSPersistentContainer(name: "CorDataModel")
+    let persistantContainer = NSPersistentContainer(name: "CoreDataModel")
     persistantContainer.loadPersistentStores { description, error in
       if let error = error {
         fatalError()
@@ -25,5 +25,27 @@ final class CoredataManager {
   
   private var viewContext: NSManagedObjectContext {
     return persistantContainer.viewContext
+  }
+  
+  func createContext(diary: Diary) {
+    guard let diaryEntity = NSEntityDescription.entity(forEntityName: "Diary", in: viewContext) else {
+      return
+    }
+    
+    let userModel = NSManagedObject(entity: diaryEntity, insertInto: viewContext)
+    
+    userModel.setValue(diary.identifier, forKey: "identifier")
+    userModel.setValue(diary.title, forKey: "title")
+    userModel.setValue(diary.description, forKey: "description")
+    userModel.setValue(diary.createdAt, forKey: "createdAt")
+
+    guard viewContext.hasChanges else {
+      return
+    }
+    do {
+      try viewContext.save()
+    } catch {
+      print(error)
+    }
   }
 }
