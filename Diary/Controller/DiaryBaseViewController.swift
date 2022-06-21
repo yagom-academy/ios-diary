0d5c38ba9d0307d88e734a0c0aa94b42bfcc0da1
minseong <msb0987@naver.com>
2022-06-21 15:20:30 +0900

    refactor: DetailView, AddView의 공통 부분을 분리
    (cherry picked from commit baa95f2d6722fe39f7ad48c397d63f6ad4d7a634)

--- Diary/Controller/DiaryBaseViewController.swift
+++ Diary/Controller/DiaryBaseViewController.swift
@@ -8,22 +8,88 @@
 import UIKit
 
 class DiaryBaseViewController: UIViewController {
+  let bodyTextView = UITextView().then {
+    $0.font = .preferredFont(forTextStyle: .body)
+    $0.adjustsFontForContentSizeCategory = true
+  }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  private var isKeyboardShow = false
 
-        // Do any additional setup after loading the view.
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.initializeUI()
+    self.observeKeyboardNotifications()
+  }
+
+  private func initializeUI() {
+    self.view.backgroundColor = .systemBackground
+
+    let container = UIScrollView()
+    self.view.addSubview(container)
+    container.addSubview(bodyTextView)
+    container.translatesAutoresizingMaskIntoConstraints = false
+    bodyTextView.translatesAutoresizingMaskIntoConstraints = false
+
+    NSLayoutConstraint.activate([
+      container.topAnchor.constraint(
+        equalTo: view.safeAreaLayoutGuide.topAnchor,
+        constant: 8.0
+      ),
+      container.bottomAnchor.constraint(
+        equalTo: view.safeAreaLayoutGuide.bottomAnchor
+      ),
+      container.leadingAnchor.constraint(
+        equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+        constant: 8.0
+      ),
+      container.trailingAnchor.constraint(
+        equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+        constant: -8.0
+      ),
+
+      bodyTextView.topAnchor.constraint(equalTo: container.contentLayoutGuide.topAnchor),
+      bodyTextView.bottomAnchor.constraint(equalTo: container.contentLayoutGuide.bottomAnchor),
+      bodyTextView.leadingAnchor.constraint(equalTo: container.contentLayoutGuide.leadingAnchor),
+      bodyTextView.trailingAnchor.constraint(equalTo: container.contentLayoutGuide.trailingAnchor),
+      bodyTextView.widthAnchor.constraint(equalTo: container.widthAnchor),
+      bodyTextView.heightAnchor.constraint(equalTo: container.heightAnchor, constant: 1)
+    ])
+  }
+
+  private func observeKeyboardNotifications() {
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillShow(_:)),
+      name: UIResponder.keyboardWillShowNotification,
+      object: nil
+    )
+
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillHide(_:)),
+      name: UIResponder.keyboardWillHideNotification,
+      object: nil
+    )
+  }
+
+  @objc private func keyboardWillShow(_ notification: Notification) {
+    if !isKeyboardShow {
+      guard let userInfo = notification.userInfo else { return }
+      guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+
+      let contentInset = UIEdgeInsets(bottom: keyboardFrame.height)
+      self.bodyTextView.contentInset = contentInset
+      self.bodyTextView.verticalScrollIndicatorInsets = contentInset
+      self.isKeyboardShow = true
     }
-    
+  }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  @objc private func keyboardWillHide(_ notification: Notification) {
+    if isKeyboardShow {
+      let contentInset = UIEdgeInsets.zero
+      self.bodyTextView.contentInset = contentInset
+      self.bodyTextView.scrollIndicatorInsets = contentInset
+      self.isKeyboardShow = false
     }
-    */
-
+  }
 }
