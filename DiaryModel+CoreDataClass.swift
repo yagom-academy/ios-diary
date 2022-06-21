a294c63553ce5c51cfbd39c06b829313ef9934fb
Quokkaa <rktrkdml@gmail.com>
2022-06-21 17:43:54 +0900

    feat: CoreData Model 및 Subclass 생성 #7

---
+++ DiaryModel+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryModel+CoreDataClass.swift
+//  
+//
+//  Created by LIMGAUI on 2022/06/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DiaryModel)
+public class DiaryModel: NSManagedObject {
+
+}
