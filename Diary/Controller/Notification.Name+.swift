04d72be0921b3371820c3a74ebc0a5eeba4736ce
marisol <marisol.develop@gmail.com>
2022-06-21 14:55:55 +0900

    💎feat : Notification.Name extension 구현

---
+++ Diary/Controller/Notification.Name+.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.Name+.swift
+//  Diary
+//
+//  Created by 두기, marisol on 2022/06/21.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let sceneDidEnterBackgroundNotification = Notification.Name("sceneDidEnterBackgroundNotification")
+}
