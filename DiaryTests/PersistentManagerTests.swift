e6260676feb9de007ee17baf0e13a328d12e2e55
minseong <msb0987@naver.com>
2022-06-22 22:14:01 +0900

    chore: PersistentManagerTests 파일 생성

---
+++ DiaryTests/PersistentManagerTests.swift
@@ -0,0 +1,13 @@
+//
+//  PersistentManagerTests.swift
+//  DiaryTests
+//
+//  Created by Minseong, Lingo on 2022/06/22.
+//
+
+import XCTest
+@testable import Diary
+
+final class PersistentManagerTests: XCTestCase {
+  
+}
