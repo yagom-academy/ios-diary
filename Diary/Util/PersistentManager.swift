d57127103cd22426d8cedbe219a457fc22050859
minseong <msb0987@naver.com>
2022-06-22 20:20:17 +0900

    feat: PersistentManager 클래스 생성
    기본 기능 구현 및 그에 따른 로직 수정

--- Diary/Util/PersistentManager.swift
+++ Diary/Util/PersistentManager.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import CoreData
+
+final class PersistentManager {
+  static let shared = PersistentManager()
+
+  private lazy var persistentContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: "Diary")
+    container.loadPersistentStores(completionHandler: { (_, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+    return container
+  }()
+
+  private var context: NSManagedObjectContext {
+    return persistentContainer.viewContext
+  }
+
+  private init() {}
+
+  func saveContext () {
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        let nserror = error as NSError
+        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+      }
+    }
+  }
+}
