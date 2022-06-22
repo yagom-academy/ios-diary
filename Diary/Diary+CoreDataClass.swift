984d3ca1eb363e71e3387f82be94d63d6356f3b4
Donnie <seo_17@naver.com>
2022-06-22 14:59:23 +0900

    ✨Feat : CoreData 생성

---
+++ Diary/Diary+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Diary+CoreDataClass.swift
+//  
+//
+//  Created by SeoDongyeon on 2022/06/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Diary)
+public class Diary: NSManagedObject {
+
+}
---
+++ Diary/Diary+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Diary+CoreDataProperties.swift
+//  
+//
+//  Created by SeoDongyeon on 2022/06/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Diary {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Diary> {
+        return NSFetchRequest<Diary>(entityName: "Diary")
+    }
+
+    @NSManaged public var body: String?
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var title: String?
+
+}
