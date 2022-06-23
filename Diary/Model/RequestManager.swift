28d49f234c5dcc8fce688d6dbcc447ae5866f752
JoSH0318 <jsh9870@naver.com>
2022-06-23 23:06:44 +0900

    refactor: RequestManager, requestAPI 코드 develop

--- Diary/Model/RequestManager.swift
+++ Diary/Model/RequestManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 final class RequestManager {
     static let shared = RequestManager()
@@ -17,23 +18,57 @@ final class RequestManager {
         let endpoint: EndPoint = .weatherInfo(lat: lat, lon: lon)
         
         sessionManager.request(endpoint: endpoint) { data, response, error in
-            guard let data = data else {
+            guard error == nil else {
+                completion(.failure(DiaryError.networkError))
                 return
             }
             
-            // work
+            guard let response = response as? HTTPURLResponse,
+                  (200..<300).contains(response.statusCode)
+            else {
+                completion(.failure(DiaryError.networkError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(DiaryError.networkError))
+                return
+            }
+            
+            guard let decodedData = try? JSONDecoder().decode(Weather.self, from: data) else {
+                completion(.failure(DiaryError.networkError))
+                return
+            }
+            completion(.success(decodedData))
         }
     }
     
-    func requestAPI(completion: @escaping ((Result<Weather, Error>) -> Void)) {
-        let endpoint: EndPoint = .weatherIcon
+    func requestAPI(icon: String, completion: @escaping ((Result<UIImage, Error>) -> Void)) {
+        let endpoint: EndPoint = .weatherIcon(icon: icon)
         
         sessionManager.request(endpoint: endpoint) { data, response, error in
-            guard let data = data else {
+            guard error == nil else {
+                completion(.failure(DiaryError.networkError))
                 return
             }
             
-            // work
+            guard let response = response as? HTTPURLResponse,
+                  (200..<300).contains(response.statusCode)
+            else {
+                completion(.failure(DiaryError.networkError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(DiaryError.networkError))
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+                return
+            }
+            
+            completion(.success(image))
         }
     }
 }
