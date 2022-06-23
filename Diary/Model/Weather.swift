2bfdc9204c0a21787b83d642026ff733e6306fee
Grumpy <sewoong2890@gmail.com>
2022-06-23 21:31:18 +0900

    refactor: API 응답 결과로 id, icon 정보만 가져오도록 모델 수정

--- Diary/Model/Weather.swift
+++ Diary/Model/Weather.swift
@@ -7,61 +7,11 @@
 
 import Foundation
 
-struct Weather: Codable {
-    let coord: Coord
+struct Weather: Decodable {
     let weather: [WeatherElement]
-    let base: String
-    let main: Main
-    let visibility: Int
-    let wind: Wind
-    let clouds: Clouds
-    let dt: Int
-    let sys: Sys
-    let timezone, id: Int
-    let name: String
-    let cod: Int
 }
 
-struct Clouds: Codable {
-    let all: Int
-}
-
-struct Coord: Codable {
-    let lon, lat: Double
-}
-
-struct Main: Codable {
-    let temp, feelsLike, tempMin, tempMax: Double
-    let pressure, humidity: Int
-    
-    enum CodingKeys: String, CodingKey {
-        case temp
-        case feelsLike = "feels_like"
-        case tempMin = "temp_min"
-        case tempMax = "temp_max"
-        case pressure, humidity
-    }
-}
-
-struct Sys: Codable {
-    let type, id: Int
-    let message: Double
-    let country: String
-    let sunrise, sunset: Int
-}
-
-struct WeatherElement: Codable {
+struct WeatherElement: Decodable {
     let id: Int
-    let main, weatherDescription, icon: String
-    
-    enum CodingKeys: String, CodingKey {
-        case id, main
-        case weatherDescription = "description"
-        case icon
-    }
-}
-
-struct Wind: Codable {
-    let speed: Double
-    let deg: Int
+    let icon: String
 }
