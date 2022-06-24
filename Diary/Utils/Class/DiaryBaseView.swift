14426f46dfadc7dac88a6651737203e65b2a77ee
Quokkaa <rktrkdml@gmail.com>
2022-06-24 16:04:38 +0900

    refactor: View 및 ViewController의 추상클래스 생성

---
+++ Diary/Utils/Class/DiaryBaseView.swift
@@ -0,0 +1,36 @@
+//
+//  DiaryBaseView.swift
+//  Diary
+//
+//  Created by LIMGAUI on 2022/06/24.
+//
+
+import UIKit
+
+class DiaryBaseView: UIView {
+  let textView: UITextView = {
+    let textView = UITextView()
+    textView.translatesAutoresizingMaskIntoConstraints = false
+    return textView
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.configureLayout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configureLayout() {
+    self.addSubview(textView)
+    
+    NSLayoutConstraint.activate([
+      self.textView.topAnchor.constraint(equalTo: topAnchor),
+      self.textView.bottomAnchor.constraint(equalTo: bottomAnchor),
+      self.textView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+      self.textView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+    ])
+  }
+}
