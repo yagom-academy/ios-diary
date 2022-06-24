f877eb0953bdd27b9664cd2463c0694655520f62
dudu <whqtkf12@naver.com>
2022-06-24 22:26:13 +0900

    Test: 필요없는 테스트 코드 삭제

--- DiaryTests/DiaryTests.swift
+++ DiaryTests/DiaryTests.swift
@@ -9,16 +9,5 @@ import XCTest
 @testable import Diary
 
 class DiaryTests: XCTestCase {
-    func test_sample_json이_Diary배열_타입으로_decoding되는지() {
-        // given
-        let url = Bundle.main.url(forResource: "sample", withExtension: "json")!
-        let data = try! Data(contentsOf: url)
-        let targetTitle = "똘기떵이호치새초미자축인묘"
-        
-        // when
-        let diarys = try! JSONDecoder().decode([Diary].self, from: data)
-        
-        // then
-        XCTAssertEqual(diarys.first?.title, targetTitle)
-    }
+    // empty
 }
