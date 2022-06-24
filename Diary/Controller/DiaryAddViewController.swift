c7e03c629a33d89150e5b997c9614327e492a5e9
Lingo <llingo@kakao.com>
2022-06-24 16:02:17 +0900

    feat: 새 일기장을 생성하는 saveDiary 메서드 구현

--- Diary/Controller/DiaryAddViewController.swift
+++ Diary/Controller/DiaryAddViewController.swift
@@ -12,4 +12,22 @@ final class DiaryAddViewController: DiaryBaseViewController {
     super.viewDidLoad()
     bodyTextView.becomeFirstResponder()
   }
+
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    self.saveDiary()
+  }
+
+  private func saveDiary() {
+    var text = bodyTextView.text.components(separatedBy: "\n")
+    let title = text.first
+    text.removeFirst()
+    let body = text.joined(separator: "\n")
+
+    if let title = title, !title.isEmpty {
+      DiaryStorageManager.shared.create(
+        diary: Diary(title: title, body: body, createdAt: 0)
+      )
+    }
+  }
 }
