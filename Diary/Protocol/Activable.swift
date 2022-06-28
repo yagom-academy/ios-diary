8ad534848f3cf590c8b6f365be2e0d4ba8f1554b
kimt4580 <xogns8696@gmail.com>
2022-06-28 14:54:37 +0900

    ✨Feat : 공통된 ActivityViewController 사용을 위해 프로토콜 생성

---
+++ Diary/Protocol/Activable.swift
@@ -0,0 +1,21 @@
+//
+//  Activable.swift
+//  Diary
+//
+//  Created by Donnie, OneTool on 2022/06/28.
+//
+
+import UIKit
+
+protocol Activitable: UIViewController {
+}
+
+extension Activitable {
+    func activitySheet(activityItems: [Any]) {
+        let activityViewController = UIActivityViewController(
+            activityItems: activityItems,
+            applicationActivities: nil
+        )
+        self.present(activityViewController, animated: true)
+    }
+}
