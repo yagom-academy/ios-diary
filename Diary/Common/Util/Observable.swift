dbb4138c27509138192dec3aa93021b31351cb23
JoSH0318 <jsh9870@naver.com>
2022-06-28 17:10:43 +0900

    feat: Observable 타입 생성

---
+++ Diary/Common/Util/Observable.swift
@@ -0,0 +1,27 @@
+//
+//  Observable.swift
+//  Diary
+//
+//  Created by mmim, grumpy, mino on 2022/06/28.
+//
+
+import Foundation
+
+final class Observable<T> {
+    var value: T {
+        didSet {
+            self.listener?(value)
+        }
+    }
+    
+    var listener: ((T) -> Void)?
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func subscribe(listener: @escaping (T) -> Void) {
+        listener(value)
+        self.listener = listener
+    }
+}
