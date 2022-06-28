7b1e273d735e6719e41bfdeb745d51f65ab2d758
Donnie <seo_17@naver.com>
2022-06-28 12:56:56 +0900

    ✨Feat : ErrorAlertProtocol 생성, CoreDataError 정의

---
+++ Diary/Error/CoreDataError.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataError.swift
+//  Diary
+//
+//  Created by Donnie, OneTool on 2022/06/28.
+//
+
+import Foundation
+
+enum CoreDataError: String, LocalizedError {
+    case persistentContainerError
+    case saveContextError
+    case fetchError
+    
+    var errorDescription: String {
+        switch self {
+        case .persistentContainerError:
+            return "컨테이너에서 오류 바인딩을 실패하였습니다."
+        case .saveContextError:
+            return "컨텍스트 저장을 실패하였습니다."
+        case .fetchError:
+            return "컨텍스트 패치를 실패하였습니다."
+        }
+    }
+}
---
+++ Diary/Error/ErrorAlertProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorAlertProtocol.swift
+//  Diary
+//
+//  Created by Donnie, OneTool on 2022/06/28.
+//
+
+import Foundation
+
+protocol ErrorAlertProtocol {
+    var alertTitle: String { get }
+    var alertMessage: String { get }
+}
