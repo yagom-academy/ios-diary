8230124d6c09e93b237eca5bce363ac9ed3e7b5d
Lingo <llingo@kakao.com>
2022-06-28 16:13:53 +0900

    feat: PersistentStore 타입 구현 및 그에 따른 DiaryStorageManager 변경

--- Diary/Util/DiaryStorageManager.swift
+++ Diary/Util/DiaryStorageManager.swift
@@ -9,69 +9,42 @@ import Foundation
 import CoreData
 
 final class DiaryStorageManager {
-  static let shared = DiaryStorageManager()
-
-  private lazy var persistentContainer: NSPersistentContainer = {
-    let container = NSPersistentContainer(name: "Diary")
-    container.loadPersistentStores { _, error in
-      if let error = error {
-        print("PersistentManagerError - LoadContainer: ", error)
-      }
-    }
-
-    return container
-  }()
-
-  private var context: NSManagedObjectContext {
-    return self.persistentContainer.viewContext
-  }
-
-  private init() {}
-
-  func saveContext () {
-    if self.context.hasChanges {
-      do {
-        try self.context.save()
-      } catch {
-        print("PersistentManagerError - SaveContext: ", error)
-      }
-    }
-  }
+  private let storage = PersistentStore(fileName: "Diary")
 
   func create(diary: Diary) {
-    let diaryEntity = DiaryEntity(context: self.context)
+    let diaryEntity = DiaryEntity(context: self.storage.context)
     diaryEntity.setValue(diary.title, forKey: "title")
     diaryEntity.setValue(diary.body, forKey: "body")
     diaryEntity.setValue(diary.createdAt, forKey: "createdAt")
     diaryEntity.setValue(diary.uuid, forKey: "uuid")
 
-    self.saveContext()
+    self.storage.saveContext()
     NotificationCenter.default.post(name: DiaryStorageNotification.diaryWasSaved, object: nil)
   }
 
   func fetchAllDiaries() -> [DiaryEntity] {
-    guard let diaryEntities = try? context.fetch(DiaryEntity.fetchRequest()) else { return [] }
+    guard let diaryEntities = try? self.storage.context.fetch(DiaryEntity.fetchRequest()) else { return [] }
     return diaryEntities
   }
 
   func update() {
-    self.saveContext()
+    self.storage.saveContext()
     NotificationCenter.default.post(name: DiaryStorageNotification.diaryWasSaved, object: nil)
   }
 
   func delete(diary: DiaryEntity) {
-    self.context.delete(diary)
-    self.saveContext()
+    self.storage.context.delete(diary)
+    self.storage.saveContext()
     NotificationCenter.default.post(name: DiaryStorageNotification.diaryWasDeleted, object: nil)
   }
 
   func deleteAll() {
-    guard let diaryEntities = try? self.context.fetch(DiaryEntity.fetchRequest()) else { return }
+    guard let diaryEntities = try? self.storage.context.fetch(DiaryEntity.fetchRequest()) else { return }
 
     diaryEntities.forEach { entity in
-      self.context.delete(entity)
+      self.storage.context.delete(entity)
     }
 
-    self.saveContext()
+    self.storage.saveContext()
   }
 }
--- Diary/Util/PersistentStore.swift
+++ Diary/Util/PersistentStore.swift
@@ -2,7 +2,40 @@
 //  PersistentStore.swift
 //  Diary
 //
-//  Created by Lingo on 2022/06/28.
+//  Created by Minseong, Lingo on 2022/06/28.
 //
 
 import Foundation
+import CoreData
+
+final class PersistentStore {
+  private let fileName: String
+
+  init(fileName: String) {
+    self.fileName = fileName
+  }
+
+  private lazy var container: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: fileName)
+    container.loadPersistentStores { _, error in
+      if let error = error {
+        print("Can't load PersistentStore \(error)")
+      }
+    }
+    return container
+  }()
+
+  var context: NSManagedObjectContext {
+    return container.viewContext
+  }
+
+  func saveContext() {
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        print("Can't save context \(error)")
+      }
+    }
+  }
+}
