b8679ec4261914e9f4ec3103ae70af62d5d2faae
kimt4580 <xogns8696@gmail.com>
2022-06-28 14:52:06 +0900

    ♻️Refactor : ErrorAlertProtocol 파일 위치 수정

---
+++ Diary/Protocol/ErrorAlertProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorAlertProtocol.swift
+//  Diary
+//
+//  Created by Donnie, OneTool on 2022/06/28.
+//
+
+import UIKit
+
+protocol ErrorAlertProtocol: UIViewController {
+}
+
+extension ErrorAlertProtocol {
+    func showAlert(alertMessage: String) {
+        let alert = UIAlertController(title: "Error", message: alertMessage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        self.present(alert, animated: true)
+    }
+}
