c76c19b076b999c33e21c25e2044ffbd5230fed7
minseong <msb0987@naver.com>
2022-06-28 20:56:37 +0900

    feat: Weather Model 생성

--- Diary/Model/Weather.swift
+++ Diary/Model/Weather.swift
@@ -5,4 +5,11 @@
 //  Created by Minseong, Lingo on 2022/06/28.
 //
 
-import Foundation
+struct WeatherResponse: Decodable {
+  let weather: [Weather]
+}
+
+struct Weather: Decodable {
+  let main: String
+  let icon: String
+}
