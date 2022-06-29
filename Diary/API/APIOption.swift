62d2068a487b820b047640c2a336cc4dd38927f7
Taeangel <wngks2050@gmail.com>
2022-06-29 18:51:26 +0900

    refactor: 잘못된 url수정

--- Diary/API/APIOption.swift
+++ Diary/API/APIOption.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum APIOption {
   static let apiKey: String = "88db245fdcf84f6e66530a76837b25cb"
-  static let baseURL: String = "https://api.openweathermap.org/data/2.5/weather"
+  static let baseURL: String = "https://api.openweathermap.org/"
   static let iconbaseURL: String = "http://openweathermap.org/img/wn"
   static let path: String = "data/2.5/weather"
 }
