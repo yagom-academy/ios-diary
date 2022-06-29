9f49d98a8e90a3d1f78ace42c371a1d919daa9c7
Quokkaa <rktrkdml@gmail.com>
2022-06-29 11:00:35 +0900

    feat: WeatherService 구현 #17

---
+++ Diary/API/WeatherService.swift
@@ -0,0 +1,48 @@
+//
+//  WeatherService.swift
+//  Diary
+//
+//  Created by Taeangel, Quokka on 2022/06/29.
+//
+
+import CoreLocation
+
+fileprivate enum APIOption {
+  static let apiKey: String = "88db245fdcf84f6e66530a76837b25cb"
+  static let baseURL: String = "https://api.openweathermap.org/data/2.5/weather"
+  static var icon: String = ""
+  static let iconURL: String = "http://openweathermap.org/img/wn/\(icon)@2x.png"
+}
+
+final class WeatherService {
+  func fetch(
+    latitude: CLLocationDegrees,
+    longitude: CLLocationDegrees,
+    completion: @escaping (Result<WeatherResponse, NetworkError>) -> Void) {
+      
+    guard let url = URL(string: "\(APIOption.baseURL)?lat=\(latitude)&lon=\(longitude)&appid=\(APIOption.apiKey)") else {
+      return
+    }
+    
+    URLSession.shared.dataTask(with: url) { data, reponse, error in
+      guard error == nil else {
+        return completion(.failure(.wrongRequest))
+      }
+      
+      guard let httpResponse = (reponse as? HTTPURLResponse),
+            (200...300).contains(httpResponse.statusCode) else {
+        return completion(.failure(.wrongResponse))
+      }
+      
+      guard let data = data else {
+        return completion(.failure(.wrongResponse))
+      }
+      
+      guard let json = try? JSONDecoder().decode(WeatherResponse.self, from: data) else {
+        return completion(.failure(.docodingFail))
+      }
+      
+      completion(.success(json))
+    }.resume()
+  }
+}
