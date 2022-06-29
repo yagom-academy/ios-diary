0165b2ccf5cc1df34027f34d4c50231af5b8bb4c
minseong <msb0987@naver.com>
2022-06-29 20:50:40 +0900

    feat: 위도와 경도를 가지고 날씨데이터를 요청 받는 기능 구현
    info.plist 추가

--- Diary/Controller/DiaryAddViewController.swift
+++ Diary/Controller/DiaryAddViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import CoreLocation
 
 final class DiaryAddViewController: DiaryBaseViewController {
   private let storageManger: DiaryStorageManager
+  private let locationManager: CLLocationManager
+  private let repository: WeatherRepository
+  private var weather: Weather?
 
   init(storageManger: DiaryStorageManager) {
     self.storageManger = storageManger
+    self.locationManager = CLLocationManager()
+    self.repository = WeatherRepository(networkService: .init(session: .shared))
     super.init(nibName: nil, bundle: nil)
     self.observeDidEnterBackgroundNotification()
+    self.initializeLocationManager()
   }
 
   required init?(coder: NSCoder) {
@@ -41,5 +48,22 @@ final class DiaryAddViewController: DiaryBaseViewController {
 
   @objc private func createDiary() {
     self.storageManger.createDiary(text: self.bodyTextView.text)
+  }
+
+  private func initializeLocationManager() {
+    self.locationManager.requestWhenInUseAuthorization()
+    self.locationManager.startUpdatingLocation()
+    self.locationManager.delegate = self
+  }
+}
+
+extension DiaryAddViewController: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    if let coordinate = locations.last?.coordinate {
+      self.locationManager.stopUpdatingLocation()
+      self.repository.fetchWeather(lat: coordinate.latitude, lon: coordinate.longitude) { [weak self] weather in
+        self?.weather = weather
+      }
+    }
   }
 }
