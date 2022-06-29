7ff34b8fd107bda9a1e84f840000c71d53d5cc1b
Lingo <llingo@kakao.com>
2022-06-29 15:01:19 +0900

    feat: NetworkService 타입 생성 및 request 메서드 구현

--- Diary/Network/NetworkService.swift
+++ Diary/Network/NetworkService.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+  case invalidateEndpoint
+  case invalidateRequest
+  case invalidateResponse
+  case internalServerError
+}
+
+final class NetworkService {
+  private let session: URLSession
+
+  init(session: URLSession) {
+    self.session = session
+  }
+
+  func request(endpoint: Requestable, completion: @escaping (Result<Data, Error>) -> Void) {
+    guard let urlRequest = endpoint.createRequest() else {
+      completion(.failure(NetworkError.invalidateEndpoint))
+      return
+    }
+
+    self.session.dataTask(with: urlRequest) { data, response, error in
+      guard error == nil, let data = data else {
+        completion(.failure(NetworkError.invalidateRequest))
+        return
+      }
+      guard let response = response as? HTTPURLResponse else {
+        completion(.failure(NetworkError.invalidateResponse))
+        return
+      }
+      guard (200..<300).contains(response.statusCode) else {
+        completion(.failure(NetworkError.internalServerError))
+        return
+      }
+      completion(.success(data))
+    }.resume()
+  }
+}
