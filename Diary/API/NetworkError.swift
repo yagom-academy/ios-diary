2fb9a24901d9566d0030f7c0a438513238b3e8c3
Quokkaa <rktrkdml@gmail.com>
2022-06-29 11:00:59 +0900

    feat: NetworkError 구현 #17

---
+++ Diary/API/NetworkError.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkError.swift
+//  Diary
+//
+//  Created by Taeangel, Quokka on 2022/06/29.
+//
+
+enum NetworkError: Error {
+  case wrongRequest
+  case wrongResponse
+  case docodingFail
+}
