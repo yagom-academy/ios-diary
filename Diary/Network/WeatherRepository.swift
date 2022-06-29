892582b37db63589ee787954da2768501a0ed21d
Lingo <llingo@kakao.com>
2022-06-29 16:17:42 +0900

    feat: WeatherRepository 타입 생성 및 fetchWeather 메서드 구현

--- Diary/Network/WeatherRepository.swift
+++ Diary/Network/WeatherRepository.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+final class WeatherRepository {
+  private let networkService: NetworkService
+
+  init(networkService: NetworkService) {
+    self.networkService = networkService
+  }
+  
+  func fetchWeather(lat: Double, lon: Double, completion: @escaping (Weather) -> Void) {
+    let endpoint = APIEndpoint.fetchWeather(lat: lat, lon: lon)
+    self.networkService.request(endpoint: endpoint) { result in
+      switch result {
+      case .success(let data):
+        guard let weatherData = Parser.decode(WeatherResponse.self, data: data) else { return }
+        guard let weather = weatherData.weather.first else { return }
+        completion(weather)
+      case .failure(let error):
+        print(error.localizedDescription)
+      }
+    }
+  }
+}
