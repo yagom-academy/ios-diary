8c3e566e9ede3c99b29ff3a25d6643a9bd6cbad6
Lingo <llingo@kakao.com>
2022-06-29 16:54:25 +0900

    feat: UIImageView setImage(iconID:) 메서드에 Cancellable 적용

--- Diary/Util/Extension/UIImageView+Extension.swift
+++ Diary/Util/Extension/UIImageView+Extension.swift
@@ -8,16 +8,17 @@
 import UIKit
 
 extension UIImageView {
-  func setImage(iconID: String) {
+  func setImage(iconID: String) -> Cancellable? {
     let service = NetworkService(session: .shared)
     let endpoint = APIEndpoint.fetchWeatherIcon(iconID: iconID)
 
-    service.request(endpoint: endpoint) { result in
+    let cancellable = service.request(endpoint: endpoint) { result in
       guard case let .success(data) = result else { return }
 
       DispatchQueue.main.async {
         self.image = UIImage(data: data)
       }
     }
+    return cancellable
   }
 }
