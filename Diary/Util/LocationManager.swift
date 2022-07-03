d7b7fac7dc63a1e2af77357866b0d96ee99e22dd
Lingo <llingo@kakao.com>
2022-07-03 15:35:16 +0900

    feat: LocationManager 타입 생성 및 fetchWeather 메서드 구현

--- Diary/Util/LocationManager.swift
+++ Diary/Util/LocationManager.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import CoreLocation
+
+final class LocationManager: NSObject {
+  private let clLocationManager: CLLocationManager
+  private let weatherService: WeatherService
+  private var coordinate: CLLocationCoordinate2D?
+
+  override init() {
+    self.weatherService = WeatherService(networking: .init(session: .shared))
+    self.clLocationManager = CLLocationManager()
+    super.init()
+    self.clLocationManager.requestWhenInUseAuthorization()
+    self.clLocationManager.startUpdatingLocation()
+    self.clLocationManager.delegate = self
+  }
+
+  func fetchWeather(completion: @escaping (Weather) -> Void) {
+    guard let coordinate = coordinate else { return }
+    self.weatherService.fetchWeather(lat: coordinate.latitude, lon: coordinate.longitude) { weather in
+      completion(weather)
+    }
+  }
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension LocationManager: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let coordinate = locations.last?.coordinate else { return }
+    self.coordinate = coordinate
+    self.clLocationManager.stopUpdatingLocation()
+  }
+}
