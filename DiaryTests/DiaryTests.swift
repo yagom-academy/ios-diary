5f65ba6f2a2c6b5f4a042209d4e8be5a74c235a6
Hugh-github <tnghk4822@gmail.com>
2022-08-16 16:44:30 +0900

    test: NetworkingProvider 테스트 코드 구현

---
+++ DiaryTests/DiaryTests.swift
@@ -0,0 +1,72 @@
+//
+//  DiaryTests.swift
+//  DiaryTests
+//
+//  Created by dhoney96 on 2022/08/16.
+//
+
+import XCTest
+@testable import Diary
+
+class DiaryTests: XCTestCase {
+    var sut: NetworkingProvider?
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = NetworkingProvider()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_JSON파일을_호출할때_DiaryContent타입의_Data반환() {
+        //given
+        let fileName = "diarySample"
+        let expectation = "똘기떵이호치새초미자축인묘"
+        let decodedData = sut?.requestAndDecode(fileName, dataType: [DiaryContent].self)
+        
+        //when
+        switch decodedData {
+        case .success(let contents):
+            //then
+            let result = contents[0].title
+            XCTAssertEqual(expectation, result)
+        default:
+            return
+        }
+    }
+    
+    func test_JSON파일_이름이_다를때_JSONError를_반환() {
+        //given
+        let fileName = "diary"
+        let expectation = JSONError.noneFile
+        let decodedData = sut?.requestAndDecode(fileName, dataType: [DiaryContent].self)
+        
+        //when
+        switch decodedData {
+        case .failure(let error):
+            //then
+            XCTAssertEqual(expectation, error)
+        default:
+            return
+        }
+    }
+    
+    func test_JSON파일_디코딩이_실패할때_JSONError를_반환() {
+        //given
+        let fileName = "diarySample"
+        let expectation = JSONError.decodingFailure
+        let decodedData = sut?.requestAndDecode(fileName, dataType: DiaryContent.self)
+        
+        //when
+        switch decodedData {
+        case .failure(let error):
+            //then
+            XCTAssertEqual(expectation, error)
+        default:
+            return
+        }
+    }
+}
