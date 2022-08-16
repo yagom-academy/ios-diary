5e06a8fb86bc53308a413769eb6384f28797a21b
gjrjs31@naver.com <gjrjs31@naver.com>
2022-08-16 19:47:15 +0900

    feat: JsonDeconder 기능 Extension으로 구현

---
+++ Diary/Model/Json/JSONDecoder + Extension.swift
@@ -0,0 +1,26 @@
+//
+//  JSONDecoder + Extension.swift
+//  Diary
+//
+//  Created by 허건 on 2022/08/16.
+//
+
+import Foundation
+
+extension JSONDecoder {
+    static func decodedJson<T: Decodable>(jsonName: String) -> T? {
+        let decoder = JSONDecoder()
+        
+        guard let fileLocation = Bundle.main.url(forResource: jsonName, withExtension: "json") else {
+            return nil
+        }
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            let sampleDiary = try decoder.decode(T.self, from: data)
+            return sampleDiary
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+}
---
+++ Diary/Model/Json/SampleJson.swift
@@ -0,0 +1,18 @@
+//
+//  SampleJson.swift
+//  Diary
+//
+//  Created by Kiwi, Brad. on 2022/08/16.
+//
+
+import Foundation
+
+struct SampleJson: Codable {
+    let title, body: String
+    let createdAt: Int
+
+    enum CodingKeys: String, CodingKey {
+        case title, body
+        case createdAt = "created_at"
+    }
+}
