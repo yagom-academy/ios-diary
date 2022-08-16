00047b63ee96adc61a53dd09754e84df4daa33ff
finnn1 <tgss3535@gmail.com>
2022-08-16 17:03:32 +0900

    feat: TableView 기능 구현

--- Diary/Controllers/DiaryViewController.swift
+++ Diary/Controllers/DiaryViewController.swift
@@ -1,15 +1,91 @@
 //
 //  Diary - ViewController.swift
-//  Created by yagom. 
+//  Created by Finnn, 수꿍 
 //  Copyright © yagom. All rights reserved.
 // 
 
 import UIKit
 
 class DiaryViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var tableView: UITableView?
+    var dataSource: UITableViewDiffableDataSource<Section, DiarySampleData>?
+    let diarySampleData: [DiarySampleData]? = JSONData.parse(name: "sample")
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        configureTableView()
+        registerTableView()
+        configureDataSource()
+        configureUI()
+    }
+
+    // MARK: - Methods
+    
+    private func configureTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView?.delegate = self
+        
+        guard let tableView = tableView else {
+            return
+        }
+        
+        view.addSubview(tableView)
+    }
+    
+    private func configureDataSource() {
+        guard let tableView = tableView,
+              let diarySampleData = self.diarySampleData else {
+            return
+        }
+        
+        dataSource = UITableViewDiffableDataSource<Section, DiarySampleData>(tableView: tableView, cellProvider: { tableView, indexPath, item in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath)
+            cell.textLabel?.text = item.title
+            
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, DiarySampleData>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(diarySampleData)
+        
+        dataSource?.apply(snapshot)
+    }
+    
+    private func registerTableView() {
+        guard let tableView = tableView else {
+            return
+        }
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CustomCell")
+        tableView.dataSource = dataSource
+    }
+    
+    private func configureUI() {
+        guard let tableView = tableView else {
+            return
+        }
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
+
+extension DiaryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath)
     }
 }
