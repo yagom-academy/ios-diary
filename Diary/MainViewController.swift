a96d27817c6910466d6db786d58b251b10a3cf8e
웡빙 <wb277@naver.com>
2022-08-16 15:52:17 +0900

    feat: TableView생성 및 네이게이션 바 구성

---
+++ Diary/MainViewController.swift
@@ -0,0 +1,54 @@
+//
+//  Diary - MainViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class MainViewController: UIViewController {
+    private var tableView = UITableView(frame: .zero)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+        configureNavigationBar()
+        configureTableView()
+    }
+    
+    private func configureNavigationBar() {
+        self.navigationItem.title = "일기장"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonDidTapped))
+    }
+    
+    private func configureTableView() {
+        self.view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = self.view.bounds
+    }
+    
+    @objc func addButtonDidTapped() {
+        
+    }
+}
+
+extension MainViewController: UITableViewDelegate {
+    
+}
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
+
