42ac92eb33ded32a5373e32b0dfe463deba59b12
minsson <mskaang@gmail.com>
2022-08-16 16:24:15 +0900

    build: 스토리보드 삭제 및 코드 기반으로 프로젝트 초기 설정
    - 폴더 구조 MVC 패턴에 맞게 변경
    - UI를 코드로 구현하기 위해 불필요한 스토리보드 파일 제거

---
+++ Diary/Resources/Sources/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  Diary - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    // MARK: - Life Cycles
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - Private Methods
+
+private extension ViewController {
+
+    func configureRootViewUI() {
+        view.backgroundColor = .white
+    }
+}
