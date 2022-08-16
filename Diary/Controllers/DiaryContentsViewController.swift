4ec90752d693103a3b8059071bf32600300073eb
MinsuJeon <chso294@naver.com>
2022-08-16 19:59:17 +0900

    feat: keyboard 유무에 따른 textView contentInset 조정

--- Diary/Controllers/DiaryContentsViewController.swift
+++ Diary/Controllers/DiaryContentsViewController.swift
@@ -20,3 +20,37 @@ class DiaryContentsViewController: UIViewController {
         super.viewDidLoad()
 
         title = Date().formatToStringDate()
+        configureNotificationCenter()
+    }
+    
+    // MARK: - Methods
+    
+    private func configureNotificationCenter() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+              let view = view as? DiaryContentView else {
+            return
+        }
+        
+        let contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.size.height, right: 0.0)
+        
+        view.textView.contentInset = contentInset
+        view.textView.scrollIndicatorInsets = contentInset
+    }
+    
+    @objc private func keyboardWillHide() {
+        guard let view = view as? DiaryContentView else {
+            return
+        }
+        
+        let contentInset = UIEdgeInsets.zero
+        
+        view.textView.contentInset = contentInset
+        view.textView.scrollIndicatorInsets = contentInset
+    }
+}
