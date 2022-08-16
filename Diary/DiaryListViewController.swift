d067bbe499ff05014547379e8d6f4798c0b3dd09
apwierk2451 <apwierk2451@gmail.com>
2022-08-16 18:45:44 +0900

    feat: CollectionView 생성

--- Diary/DiaryListViewController.swift
+++ Diary/DiaryListViewController.swift
@@ -7,8 +7,69 @@
 import UIKit
 
 class DiaryListViewController: UIViewController {
+    var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
+        collectionView = setupCollectionView(frame: .zero,
+                                             collectionViewLayout: setupLayout())
     }
+    
+    private func setupCollectionView(frame: CGRect, collectionViewLayout: UICollectionViewLayout) -> UICollectionView {
+        let collectionView = UICollectionView(frame: frame, collectionViewLayout: collectionViewLayout)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        setupCollectionViewConstraints(collectionView)
+        
+        return collectionView
+    }
+    
+    private func setupCollectionViewConstraints(_ collectionView: UICollectionView) {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    private func setupLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(0.1))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitem: item,
+                                                       count: 1)
+        group.interItemSpacing = .fixed(10)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
+
+extension DiaryListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = UICollectionViewCell()
+        cell.contentView.backgroundColor = .gray
+        return cell
+    }
+}
+
+extension DiaryListViewController: UICollectionViewDelegate {
+    
 }
