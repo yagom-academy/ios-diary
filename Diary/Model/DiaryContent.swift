7c036c45e4c5c56fd050697710ceb44e3c7de1e9
derrickkim0109 <derrickkim0109@naver.com>
2022-08-16 15:27:52 +0900

    feat: "DiaryContent" Model 타입 생성

---
+++ Diary/Model/DiaryContent.swift
@@ -0,0 +1,14 @@
+//
+//  DiaryContent.swift
+//  Diary
+//
+//  Created by Derrick kim on 2022/08/16.
+//
+
+import Foundation
+
+struct DiaryContent {
+    let title: String
+    let body: String
+    let createdAt: String
+}
