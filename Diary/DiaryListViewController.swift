3fbca7069f7cfd17d30d4ea35634ddf84118133e
bard <schy2503@naver.com>
2022-08-16 17:24:02 +0900

    style:  폴더 정리

--- Diary/DiaryListViewController.swift
+++ Diary/DiaryListViewController.swift
@@ -1,18 +1,14 @@
 //
-//  Diary - ViewController.swift
+//  Diary - DiaryListViewController.swift
 //  Created by bonf, bard. 
 //  Copyright © yagom. All rights reserved.
 // 
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class DiaryListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
     }
-
-
 }
-
--- Diary/Resources/SceneDelegate.swift
+++ Diary/Resources/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let mainViewContoroller = ViewController()
+        let mainViewContoroller = DiaryListViewController()
         let navigationController = UINavigationController(rootViewController: mainViewContoroller)
         
         window?.rootViewController = navigationController
