3352ea1cf0e44c70d33a512552296e0a070c6ff1
finnn1 <tgss3535@gmail.com>
2022-08-16 18:25:37 +0900

    feat: DateFormatter를 활용하여 날짜 Int값을 String으로 변환해주는 메서드 구현

---
+++ Diary/Models/Int+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  Int+Extension.swift
+//  Diary
+//
+//  Created by Finnn, 수꿍 on 2022/08/16.
+//
+
+import Foundation
+
+extension Int {
+    func formatToStringDate() -> String? {
+        let timeInterval = TimeInterval(self)
+        let myNSDate = Date(timeIntervalSince1970: timeInterval)
+        
+        let dateFormatter = DateFormatter()
+        
+        guard let localeID = Locale.preferredLanguages.first,
+              let deviceLocale = Locale(identifier: localeID).languageCode else { return nil }
+        
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        
+        dateFormatter.locale = Locale(identifier: deviceLocale)
+        dateFormatter.timeZone = TimeZone.current
+        
+        return dateFormatter.string(from: myNSDate)
+    }
+}
