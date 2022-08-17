234fd712a6f12c2f493edc11a2e76417581b234a
ic_6_user_28 <toskw@naver.com>
2022-08-17 10:58:50 +0900

    feat: 메인 테이블 뷰 셀 구현

--- Diary/View/MainTableViewCell.swift
+++ Diary/View/MainTableViewCell.swift
@@ -2,22 +2,71 @@
 //  MainTableViewCell.swift
 //  Diary
 //
-//  Created by Kiwon Song on 2022/08/17.
+//  Created by Kiwi, Brad on 2022/08/17.
 //
 
 import UIKit
 
 class MainTableViewCell: UITableViewCell {
+    private let diaryTitle: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let diaryBody: UILabel = {
+        let label = UILabel()
+        return label
+    }()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private let diaryDate: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let horizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        
+        return stackView
+    }()
+    
+    private let verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 5
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        accessoryType = .disclosureIndicator
+        configureViews()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
-
+    
+    private func configureViews() {
+        self.contentView.addSubview(verticalStackView)
+        
+        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        [diaryDate, diaryBody].forEach {
+            horizontalStackView.addArrangedSubview($0)
+        }
+        
+        [diaryTitle, horizontalStackView].forEach {
+            verticalStackView.addArrangedSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            verticalStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            verticalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            verticalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            verticalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
 }
