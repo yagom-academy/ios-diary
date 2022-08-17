71937845996d4f62a1b7004e1f9c297afdde3683
derrickkim0109 <derrickkim0109@naver.com>
2022-08-17 14:57:56 +0900

    feat: 현재 날짜 포맷 메서드 구현

--- Diary/View/AddDiaryViewController.swift
+++ Diary/View/AddDiaryViewController.swift
@@ -2,7 +2,7 @@
 //  AddDiaryViewController.swift
 //  Diary
 //
-//  Created by dhoney96 on 2022/08/17.
+//  Created by Hugh, Derrick on 2022/08/17.
 //
 
 import UIKit
@@ -11,5 +11,16 @@ final class AddDiaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        
+        self.title = fetchCurrentDate()
+    }
+    
+    private func fetchCurrentDate() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        
+        return dateFormatter.string(from: date)
     }
 }
