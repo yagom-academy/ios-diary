a9bbc2a44312a0e8be47555841ca06ab86eaf75b
Hugh-github <tnghk4822@gmail.com>
2022-08-17 14:06:12 +0900

    feat: AddDiaryViewController 생성

---
+++ Diary/View/AddDiaryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddDiaryViewController.swift
+//  Diary
+//
+//  Created by dhoney96 on 2022/08/17.
+//
+
+import UIKit
+
+final class AddDiaryViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
