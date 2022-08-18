bac393b9390ed8cc540ff9fe6805264731013e24
MinsuJeon <chso294@naver.com>
2022-08-18 18:12:48 +0900

    feat: 매직 리터럴 제거를 위한 별도의 Namespaes 파일 생성

---
+++ Diary/Namespaces/AlertMessage.swift
@@ -0,0 +1,11 @@
+//
+//  AlertMessage.swift
+//  Diary
+//
+//  Created by 전민수 on 2022/08/18.
+//
+
+enum AlertMessage {
+    static let errorAlertTitle = "Error!"
+    static let confirmActionTitle = "확인"
+}
