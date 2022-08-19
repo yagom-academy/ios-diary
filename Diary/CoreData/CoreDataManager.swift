83254c37b7f828dae708e4826a1d5d0e61b6d67d
BaekGom <jihunmm02@gmail.com>
2022-08-19 12:00:51 +0900

    feat: CoreData를 관리하는 싱글턴 객체인  CoreDataManager 구현
    - 저장: saveDiary
    - 불러오기: fetchDiary

---
+++ Diary/CoreData/CoreDataManager.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataManager.swift
+//  Diary
+//
+//  Created by 백곰,주디 on 2022/08/19.
+//
+
+import CoreData
+import UIKit
+
+class CoreDataManager {
+    static let shared = CoreDataManager()
+    
+    private init() {
+        
+    }
+    
+    func saveDiary(item: DiaryItem) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let diaryEntity = DiaryEntity(context: context)
+        
+        diaryEntity.setValue(item.title, forKey: "title")
+        diaryEntity.setValue(item.body, forKey: "body")
+        diaryEntity.setValue(item.createdAt, forKey: "createdAt")
+        
+        appDelegate.saveContext()
+    }
+    
+    func fetchDiary() -> [DiaryEntity]? {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return nil }
+        let context = appDelegate.persistentContainer.viewContext
+        
+        do {
+            let diary = try context.fetch(DiaryEntity.fetchRequest())
+            return diary
+        } catch {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
+}
