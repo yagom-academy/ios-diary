ae8eda96d55e2791570feb483c649fb6787e1a2c
MinsuJeon <chso294@naver.com>
2022-08-23 22:14:58 +0900

    refactor: CoreDataManager 클래스에 final 키워드 추가

--- Diary/Models/CoreDataManager.swift
+++ Diary/Models/CoreDataManager.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreData
 
-class CoreDataManager {
+final class CoreDataManager {
     static let shared = CoreDataManager()
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     let persistentContainer: NSPersistentContainer = {
