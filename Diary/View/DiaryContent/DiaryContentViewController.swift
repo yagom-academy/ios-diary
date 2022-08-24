c2aa281f4bfce4a1477ba7f7dba89be71f89e53c
Hugh-github <tnghk4822@gmail.com>
2022-08-24 13:51:16 +0900

    feat: activityView, actionSheet, alert 추가

--- Diary/View/DiaryContent/DiaryContentViewController.swift
+++ Diary/View/DiaryContent/DiaryContentViewController.swift
@@ -28,6 +28,58 @@ final class DiaryContentViewController: UIViewController {
     private func setupDefault() {
         self.view.backgroundColor = .white
         self.title = diaryViewModel.dateText
+        
+        let rightButtonImage = UIImage(systemName: "ellipsis.circle")
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: rightButtonImage,
+                                                                         style: .plain,
+                                                                         target: self,
+                                                                         action: #selector(didTappedEllipsisButton))
+    }
+    
+    @objc func didTappedEllipsisButton() {
+        presentActionSheet()
+    }
+    
+    private func presentActionSheet() {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let shareAction = UIAlertAction(title: "Share...", style: .default) { [weak self]_ in
+            self?.presentActivityView()
+        }
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { [weak self]_ in
+            self?.presentDeleteAlert()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(shareAction)
+        alertController.addAction(deleteAction)
+        alertController.addAction(cancelAction)
+        
+        alertController.modalPresentationStyle = .overFullScreen
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func presentActivityView() {
+        guard let sharingText = diaryDescriptionTextView.text else {
+            return
+        }
+        
+        let activityViewController = UIActivityViewController(activityItems: [sharingText], applicationActivities: nil)
+        present(activityViewController, animated: true, completion: nil)
+    }
+    
+    private func presentDeleteAlert() {
+        let alertController = UIAlertController(title: "진짜요?", message: "정말로 삭제하시겠어요?", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "취소", style: .default, handler: nil)
+        let deleteAction = UIAlertAction(title: "삭제", style: .destructive) { [weak self] _ in
+            self?.diaryViewModel.delete((self?.diaryViewModel.titleText)!)
+            self?.navigationController?.popViewController(animated: true)
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(deleteAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     private func configureTextView() {
