07f69a0a67c3ebd83a37b86db484d6b8a341cbe0
Hugh-github <tnghk4822@gmail.com>
2022-08-29 21:23:58 +0900

    feat: APIConfiguration 구현

---
+++ Diary/Network/API/APIConfiguration.swift
@@ -0,0 +1,16 @@
+//
+//  APIConfiguration.swift
+//  Diary
+//
+//  Created by Hugh, Derrick on 2022/08/29.
+//
+
+import Foundation
+
+struct APIConfiguration {
+    let url: URL
+    
+    init(url: URL) {
+        self.url = url
+    }
+}
