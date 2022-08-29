01c22ca6f5750fd1557d7370d206dfd4f2cedf20
Hugh-github <tnghk4822@gmail.com>
2022-08-29 21:24:22 +0900

    feat: APIManager 구현

--- Diary/Network/API/APIManager.swift
+++ Diary/Network/API/APIManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct APIManager: GETProtocol {
+    var configuration: APIConfiguration
+
+    init?(url: String, latitude: Double, longitude: Double) {
+        guard var urlComponents = URLComponents(string: url) else {
+            return nil
+        }
+        urlComponents.queryItems = [
+            URLQueryItem(name: "lat", value: "\(latitude)"),
+            URLQueryItem(name: "lon", value: "\(longitude)"),
+            URLQueryItem(name: "appid", value: "")]
+        
+        guard let url = urlComponents.url else {
+            return nil
+        }
+
+        self.configuration = APIConfiguration(url: url)
+    }
+}
