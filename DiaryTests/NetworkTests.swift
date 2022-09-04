f6866af1a9c11d2178b14fb5542566c2b500e07f
bard <schy2503@naver.com>
2022-09-04 23:43:06 +0900

    test: 불필요한 테스트 코드 제거

--- DiaryTests/NetworkTests.swift
+++ DiaryTests/NetworkTests.swift
@@ -8,30 +8,6 @@
 import XCTest
 @testable import Diary
 
-struct TestRequest: APIRequest {
-    var method: HTTPMethod  {
-        .get
-    }
-    
-    var baseURL: String {
-        URLHost.openWeather.url + path.value
-    }
-    
-    var headers: [String: String]?
-    
-    var query: [URLQueryItem]? = [
-        URLQueryItem(name: "lat", value: "35"),
-        URLQueryItem(name: "lon", value: "139"),
-        URLQueryItem(name: "appid", value: "63722b736b97508775be46f7cf76cb85")
-    ]
-    
-    var body: Data?
-    
-    var path: URLAdditionalPath {
-        .weather
-    }
-}
-
 struct MockTestRequest: APIRequest {
     var method: HTTPMethod {
         .get
@@ -57,19 +33,16 @@ struct MockTestRequest: APIRequest {
 
 
 class NetworkTests: XCTestCase {
-    var sut: TestRequest?
-    var sut2: MockTestRequest?
+    var sut: MockTestRequest?
     
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut = TestRequest()
-        sut2 = MockTestRequest()
+        sut = MockTestRequest()
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         sut = nil
-        sut2 = nil
     }
     
     func test_mockRequest를통해_데이터를_잘받아오는지() {
@@ -77,7 +50,7 @@ class NetworkTests: XCTestCase {
         let expectation = expectation(description: "wait")
         let mockSession = MockSession()
         var main: String?
-        guard let sut2 = sut2 else { return }
+        guard let sut2 = sut else { return }
         
         mockSession.dataTask(with: sut2) { (result: Result<WeatherModel, Error>) in
             switch result {
@@ -92,32 +65,6 @@ class NetworkTests: XCTestCase {
         
         // when
         let result = "Rain"
-        
-        // then
-        XCTAssertEqual(main, result)
-    }
-    
-    func test_urlRequest를통해_데이터를_잘받아오는지() {
-        // given
-        let expectation = expectation(description: "wait")
-        let weatherSession = DiaryURLSession()
-        var main: String?
-        
-        guard let sut = sut else { return }
-        
-        weatherSession.dataTask(with: sut) { (result: Result<WeatherModel, Error>) in
-            switch result {
-            case .success(let success):
-                main = success.weather[0].main
-            case .failure(let failure):
-                print(failure)
-            }
-            expectation.fulfill()
-        }
-        wait(for: [expectation], timeout: 300)
-        
-        // when
-        let result = "Clouds"
         
         // then
         XCTAssertEqual(main, result)
