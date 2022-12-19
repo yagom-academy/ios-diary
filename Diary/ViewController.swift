5d18a9224bab69633450d677c5d7c15fb6723781
lxodud <xodud1998223@naver.com>
2022-12-19 19:08:04 +0900

    :lipstick: tableView 요소 생성

--- Diary/ViewController.swift
+++ Diary/ViewController.swift
@@ -6,12 +6,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    private let tableView: UITableView = UITableView(frame: .zero, style: .plain)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         navigationItem.title = "example"
+        configureTableViewLayout()
+    }
+
+    private func configureTableViewLayout() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
     }
 }
