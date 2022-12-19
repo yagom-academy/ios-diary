eebef5cb2ea2523dfc2fef4be908c9332651c127
Kyo <hyosing92@gmail.com>
2022-12-19 18:46:40 +0900

    feat : Diary DTO 생성

---
+++ Diary/Models/Diary.swift
@@ -0,0 +1,19 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Kyo, Baem on 2022/12/19.
+//
+
+import Foundation
+
+struct Diary: Decodable {
+    var title: String
+    var body: String
+    var createdAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title, body
+        case createdAt = "created_at"
+    }
+}
