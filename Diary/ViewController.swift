de6349ecff70fe8091c7698a2f538c9c491a65b2
미니 <cow970814@naver.com>
2022-12-19 19:18:07 +0900

    :lipstick: TableView Diffable DataSource 설정 및 임시 데이터 추가

--- Diary/ViewController.swift
+++ Diary/ViewController.swift
@@ -8,6 +8,7 @@ import UIKit
 
 final class ViewController: UIViewController {
     private let tableView: UITableView = UITableView(frame: .zero, style: .plain)
+    private var dataSource: UITableViewDiffableDataSource<Int, Int>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +16,9 @@ final class ViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationItem.title = "example"
         configureTableViewLayout()
+        
+        setUpTableViewDataSource()
+        setTableViewData()
     }
 
     private func configureTableViewLayout() {
@@ -30,4 +34,32 @@ final class ViewController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
         ])
     }
+    
+    private func setUpTableViewDataSource() {
+        tableView.register(CustomCell.self, forCellReuseIdentifier: CustomCell.identifier)
+        dataSource = UITableViewDiffableDataSource<Int, Int>(
+            tableView: tableView
+        ) { tableView, indexPath, item in
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: CustomCell.identifier,
+                for: indexPath
+            ) as? CustomCell else {
+                return UITableViewCell()
+            }
+            cell.textLabel?.text = item.description
+            
+            return cell
+        }
+    }
+    
+    private func setTableViewData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(Array(1...10))
+        dataSource?.apply(snapshot)
+    }
+}
+
+class CustomCell: UITableViewCell {
+    static let identifier = String(describing: CustomCell.self)
 }
