ac4428d08a78fe5de1f117dfc4d52233e00a8cc1
jonghancha <jonghancha@gmail.com>
2022-12-20 11:40:30 +0900

    feat: 네비게이션바를 구성하기 위해 configureNavigationBar()메서드 추가

--- Diary/Controller/ViewController.swift
+++ Diary/Controller/ViewController.swift
@@ -10,7 +10,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigationBar()
         // Do any additional setup after loading the view.
+    }
+    
+    private func configureNavigationBar() {
+        self.navigationItem.title = "일기장"
+        configureDoneButton()
+    }
+    
+    private func configureDoneButton() {
+        let addItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        self.navigationItem.rightBarButtonItem = addItem
     }
 
 }
