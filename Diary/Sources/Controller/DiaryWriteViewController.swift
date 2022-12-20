7dee915d99f33b1ed55c07f88d3694d35e81d277
미니 <cow970814@naver.com>
2022-12-20 19:26:50 +0900

    :sparkles: 일기 작성에 대한 Controller 생성

--- Diary/Sources/Controller/DiaryWriteViewController.swift
+++ Diary/Sources/Controller/DiaryWriteViewController.swift
@@ -7,9 +7,60 @@
 import UIKit
 
 final class DiaryWriteViewController: UIViewController {
+    let titleTextField: UITextField = {
+        let titleTextField = UITextField()
+        titleTextField.placeholder = "제목을 입력해주세요."
+        titleTextField.font = .preferredFont(forTextStyle: .title2)
+        titleTextField.adjustsFontForContentSizeCategory = true
+        return titleTextField
+    }()
+    
+    let contentTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .preferredFont(forTextStyle: .body)
+        textView.adjustsFontForContentSizeCategory = true
+        
+        return textView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setNavigationBar()
+        configureLayout()
+    }
+}
+
+extension DiaryWriteViewController {
+    private func addElementViews() {
+        [titleTextField, contentTextView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
     
+    private func configureLayout() {
+        addElementViews()
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            titleTextField.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            titleTextField.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 8),
+            titleTextField
+                .trailingAnchor
+                .constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            
+            contentTextView.leadingAnchor.constraint(equalTo: titleTextField.leadingAnchor),
+            contentTextView
+                .topAnchor
+                .constraint(equalTo: titleTextField.bottomAnchor, constant: 8),
+            contentTextView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            contentTextView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+    
+    private func setNavigationBar() {
         navigationItem.setNavigationTitle(title: Date().description)
     }
 }
