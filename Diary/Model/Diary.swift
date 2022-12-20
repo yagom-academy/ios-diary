c9995668175c4894379751aaf6a7e319b01f2948
ash-youu <ash.you.dev@gmail.com>
2022-12-20 11:21:52 +0900

    feat: 메인 스토리보드 삭제 및 JSON 모델 타입 정의

---
+++ Diary/Model/Diary.swift
@@ -0,0 +1,17 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 애종, 애쉬 on 2022/12/20.
+//
+
+struct Diary: Decodable {
+    let title: String
+    let body: String
+    let createdAt: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case title, body
+        case createdAt = "created_at"
+    }
+}
