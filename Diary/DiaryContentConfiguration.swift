4fba0adf7d9177795784d32d3bc45d56304a70b0
lxodud <xodud1998223@naver.com>
2022-12-20 11:35:27 +0900

    :lipstick: 커스텀 DiaryContentConfiguration 구현

---
+++ Diary/DiaryContentConfiguration.swift
@@ -0,0 +1,73 @@
+//
+//  DiaryContentConfiguration.swift
+//  Diary
+//
+//  Created by 이태영 on 2022/12/20.
+//
+
+import UIKit
+
+struct DiaryContentConfiguration: UIContentConfiguration, Hashable {
+    var headerString: String?
+    var dateString: String?
+    var bodyString: String?
+    
+    func makeContentView() -> UIView & UIContentView {
+        return DiaryContentView(configuration: self)
+    }
+    
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
+
+class DiaryContentView: UIView, UIContentView {
+    let headerLabel = UILabel()
+    let dateLabel = UILabel()
+    let bodyLabel = UILabel()
+    private var appliedConfiguration: DiaryContentConfiguration?
+    
+    init(configuration: DiaryContentConfiguration) {
+        super.init(frame: .zero)
+        setupInternalViews()
+        apply(configuration: configuration)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var configuration: UIContentConfiguration {
+        get {
+            return appliedConfiguration ?? DiaryContentConfiguration()
+        }
+        set {
+            guard let newConfig = newValue as? DiaryContentConfiguration else { return }
+            apply(configuration: newConfig)
+        }
+    }
+    
+    private func setupInternalViews() {
+        let insideStackView = UIStackView(arrangedSubviews: [dateLabel, bodyLabel])
+        let totalStackView = UIStackView(arrangedSubviews: [headerLabel, insideStackView])
+        totalStackView.axis = .vertical
+        totalStackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(totalStackView)
+        
+        NSLayoutConstraint.activate([
+            totalStackView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            totalStackView.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            totalStackView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+            totalStackView.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor)
+        ])
+    }
+    
+    private func apply(configuration: DiaryContentConfiguration) {
+        guard appliedConfiguration != configuration else { return }
+        appliedConfiguration = configuration
+        
+        headerLabel.text = configuration.headerString
+        dateLabel.text = configuration.dateString
+        bodyLabel.text = configuration.bodyString
+    }
+}
