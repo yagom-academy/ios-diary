095c00fd34c1c3720cc2037eb202b879a11bb0e2
zhilly11 <zhzh1x4@gmail.com>
2022-12-21 11:03:48 +0900

    feat: DiaryView 구현

---
+++ Diary/DiaryView.swift
@@ -0,0 +1,54 @@
+//  Diary - DiaryCell.swift
+//  Created by Ayaan, zhilly on 2022/12/21
+
+import UIKit
+
+class DiaryView: UIView {
+    private let titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = .preferredFont(forTextStyle: .title3)
+        textField.textColor = .black
+        textField.textAlignment = .left
+        textField.placeholder = "제목"
+        return textField
+    }()
+    
+    private let bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .preferredFont(forTextStyle: .body)
+        textView.textColor = .black
+        textView.textAlignment = .left
+        return textView
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    var titleText: String? { return titleTextField.text }
+    var bodyText: String? { return bodyTextView.text }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setupViews() {
+        stackView.addArrangedSubview(titleTextField)
+        stackView.addArrangedSubview(bodyTextView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+            stackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -8),
+            stackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -8)
+        ])
+    }
+}
