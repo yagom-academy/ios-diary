fc523d9369f2f9ee75cc54a8e65005a749817443
jonghancha <jonghancha@gmail.com>
2022-12-21 15:51:08 +0900

    feat: navigationBar tap시 키보드 사라지는 기능 추가, 키보드가 사라질 때 inset 변경되도록 구현

--- Diary/Controller/AddDiaryViewController.swift
+++ Diary/Controller/AddDiaryViewController.swift
@@ -16,31 +16,48 @@ final class AddDiaryViewController: UIViewController {
         self.navigationItem.title = DateFormatter().longDate
         
         self.setKeyboardObserver()
+        self.initializeHideKeyBoard()
+    }
+    
+    private func initializeHideKeyBoard() {
+        let tap: UIGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyBoard))
+        self.navigationController?.navigationBar.addGestureRecognizer(tap)
+    }
+    
+    @objc private func dismissKeyBoard() {
+        self.addDiaryView.endEditing(true)
     }
     
     func setKeyboardObserver() {
         NotificationCenter.default.addObserver(self,
-                                               selector: #selector(self.addDiaryView.keyboardWillShow),
+                                               selector: #selector(self.keyboardWillShow),
                                                name: UIResponder.keyboardWillShowNotification,
                                                object: nil)
         
         NotificationCenter.default.addObserver(self,
-                                               selector: #selector(self.addDiaryView.keyboardWillHide),
+                                               selector: #selector(self.keyboardWillHide),
                                                name: UIResponder.keyboardWillHideNotification,
                                                object: nil)
     }
     
     @objc func keyboardWillShow(notification: NSNotification) {
-        guard let userInfo: NSDictionary = notification.userInfo as? NSDictionary else { return }
-        
-        guard let keyboardFrame = userInfo.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as? NSValue else { return }
-        
-        let keyboardRectangle = keyboardFrame.cgRectValue
-        let keyboardHeight = keyboardRectangle.height
-        
-        self.textView.contentInset.bottom += keyboardHeight
+        guard let keyboardHeight = getKeyboardHeight(from: notification) else { return }
+                
+        self.addDiaryView.changeTextViewContentInset(for: keyboardHeight)
     }
     
     @objc func keyboardWillHide(notification: NSNotification) {
+        guard let keyboardHeight = getKeyboardHeight(from: notification) else { return }
+                
+        self.addDiaryView.changeTextViewContentInset(for: -keyboardHeight)
+    }
+    
+    private func getKeyboardHeight(from notification: NSNotification) -> CGFloat? {
+        guard let userInfo: NSDictionary = notification.userInfo as? NSDictionary else { return nil }
+        
+        guard let keyboardFrame = userInfo.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as? NSValue else { return nil }
+        
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        return keyboardRectangle.height
     }
 }
