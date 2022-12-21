6155f0c290e80621fd8ff36976c30dcb144d4d1d
lws2269 <lws2269@kakao.com>
2022-12-21 14:46:57 +0900

    feat: ViewController Navigation, UI 설정

--- Diary/ViewController.swift
+++ Diary/ViewController.swift
@@ -10,6 +10,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+        configureNavigationItem()
     }
+    
+    func configureUI() {
+        self.view.backgroundColor = .white
+        
+    }
+    
+    func configureNavigationItem() {
+        self.navigationItem.title = "일기장"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addDiaryPage))
+        
+    }
+    
+    @objc func addDiaryPage() { }
+    
 }
