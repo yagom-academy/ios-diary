187187b02d616128fa8003f7ad939d6a7a68dc88
lws2269 <lws2269@kakao.com>
2022-12-21 16:41:23 +0900

    refactor: cell 오토레이아웃 수정 및, 접근제어자 명시

--- Diary/DiaryCell.swift
+++ Diary/DiaryCell.swift
@@ -8,35 +8,38 @@
 import UIKit
 
 class DiaryCell: UITableViewCell {
-    
     static let identifier = "CustomCell"
     
-    let stackView: UIStackView = {
+    private let spacing: CGFloat = 10
+    
+    private let stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.distribution = .fill
         stackView.alignment = .fill
+        stackView.spacing = 10
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    let titleLabel: UILabel = {
+    private let titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "제목"
+        label.font = UIFont.preferredFont(forTextStyle: .title2)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let dateLabel: UILabel = {
+    private let dateLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "2022년 12월 21일"
         return label
     }()
     
-    let contentLabel: UILabel = {
+    private let contentLabel: UILabel = {
         let label = UILabel()
-        label.text = "첫줄첫줄첫줄첫줄첫줄첫줄첫줄"
+        label.font = UIFont.preferredFont(forTextStyle: .caption1)
+        label.textAlignment = .right
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -46,23 +49,43 @@ class DiaryCell: UITableViewCell {
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(titleLabel)
+        
         stackView.addArrangedSubview(dateLabel)
         stackView.addArrangedSubview(contentLabel)
+        contentView.addSubview(titleLabel)
         contentView.addSubview(stackView)
+        
+        configureConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been impl")
     }
     
-    func configureConstraints() {
+    func configureData(title: String, date: String, content: String) {
+        self.titleLabel.text = title
+        self.dateLabel.text = date
+        self.contentLabel.text = content
+    }
+    
+    private func configureConstraints() {
+        dateLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        contentLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: self.topAnchor),
-            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor)
+            titleLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: spacing),
+            titleLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: spacing),
+            titleLabel.widthAnchor.constraint(equalTo: self.contentView.widthAnchor, constant: -(spacing * 2)),
+            
+            stackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: spacing),
+            stackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: spacing),
+            stackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -spacing),
+            stackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -spacing),
+            
+            dateLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            contentLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor)
         ])
     }
 }
