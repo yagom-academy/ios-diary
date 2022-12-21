0344f55d5dcadab0ee4daa432ea4df1bfbd62523
lxodud <xodud1998223@naver.com>
2022-12-21 11:43:45 +0900

    :bug: dateFormatter 사용자언어로 나오지 않는 버그 수정

--- Diary/Sources/Extensions/Date +.swift
+++ Diary/Sources/Extensions/Date +.swift
@@ -9,14 +9,11 @@ import Foundation
 extension Date {
     static let dateFormatter = DateFormatter()
     
-    enum Constant {
-        static let defaultFormat = "yyyy년 MM월 dd일"
-    }
-    
-    func convertString(format: String = Constant.defaultFormat) -> String {
+    func convertString() -> String {
         let formatter = Self.dateFormatter
         formatter.timeZone = TimeZone.current
-        formatter.dateFormat = format
+        formatter.locale = Locale.current
+        formatter.dateStyle = .long
         
         return formatter.string(from: self)
     }
