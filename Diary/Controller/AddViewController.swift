656b3a07854c95d4c6ed50e0f8bb8a531767f81f
woong <workplayhard1@naver.com>
2022-12-21 20:44:28 +0900

    feat: AddViewController 생성 및 Modal 화면전환

---
+++ Diary/Controller/AddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddViewController.swift
+//  Diary
+//
+//  Created by 서현웅 on 2022/12/21.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
