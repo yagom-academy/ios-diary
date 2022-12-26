c818c5a4493dfe55121397e35089dc1a7dadfa12
Ayaan <onestar7984@gmail.com>
2022-12-26 11:29:49 +0900

    feat: DiaryDataManager Type 구현

---
+++ Diary/Source/Common/DiaryDataManager.swift
@@ -0,0 +1,16 @@
+//  Diary - DiaryDataManager.swift
+//  Created by Ayaan, zhilly on 2022/12/26
+
+struct DiaryDataManager: DiaryManageable {
+    func add(title: String, body: String, createAt: Date) {
+        <#code#>
+    }
+    
+    func remove(_ object: NSManagedObject) {
+        <#code#>
+    }
+    
+    func update(_ object: NSManagedObject) {
+        <#code#>
+    }
+}
