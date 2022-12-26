489f9319a4a64d1b43c308c7a5e05cc1f1236167
zhilly11 <zhzh1x4@gmail.com>
2022-12-26 11:56:01 +0900

    feat: DiaryDataManager add 기능 구현

--- Diary/Source/Common/DiaryDataManager.swift
+++ Diary/Source/Common/DiaryDataManager.swift
@@ -1,16 +1,29 @@
 //  Diary - DiaryDataManager.swift
 //  Created by Ayaan, zhilly on 2022/12/26
 
+import Foundation
+import CoreData
+
 struct DiaryDataManager: DiaryManageable {
     func add(title: String, body: String, createAt: Date) {
-        <#code#>
+        let context = DiaryCoreDataStack.shared.persistentContainer.viewContext
+        guard let entity = NSEntityDescription.entity(forEntityName: "DiaryData", in: context) else {
+            return
+        }
+        
+        let diary = NSManagedObject(entity: entity, insertInto: context)
+        diary.setValue(title, forKey: "title")
+        diary.setValue(body, forKey: "body")
+        diary.setValue(createAt, forKey: "createdAt")
+        
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
-    func remove(_ object: NSManagedObject) {
-        <#code#>
-    }
+    func remove(_ object: NSManagedObject) {}
     
-    func update(_ object: NSManagedObject) {
-        <#code#>
-    }
+    func update(_ object: NSManagedObject) {}
 }
