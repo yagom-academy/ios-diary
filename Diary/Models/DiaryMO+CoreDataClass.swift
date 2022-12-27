fe944bbd34e6c00e374a967a10624020cb8997b9
junho <junhoL1046@gmail.com>
2022-12-27 17:04:47 +0900

    feat: DiaryModel, NSManagedObject Subclass 생성

---
+++ Diary/Models/DiaryMO+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryMO+CoreDataClass.swift
+//  Diary
+//
+//  Created by junho lee on 2022/12/27.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DiaryMO)
+public class DiaryMO: NSManagedObject {
+
+}
--- Diary/SupportFiles/AppDelegate.swift
+++ Diary/SupportFiles/AppDelegate.swift
@@ -38,7 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "Diary")
+        let container = NSPersistentContainer(name: "DiaryModel")
         // swiftlint:disable unused_closure_parameter
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
