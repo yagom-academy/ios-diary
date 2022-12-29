73daad6dd9c1500905c8f1a209adb324f8e1bf97
summercat <summerly812@gmail.com>
2022-12-29 17:58:58 +0900

    refactor: DiaryItemManager를 구조체에서 클래스로 변경

--- Diary/Resource/Common/DiaryItemManager.swift
+++ Diary/Resource/Common/DiaryItemManager.swift
@@ -7,39 +7,39 @@
 
 import Foundation
 
-struct DiaryItemManager {
+final class DiaryItemManager {
     private var diaryItem: DiaryModel?
     
-    mutating func saveDiaryWith(title: String, body: String) {
+    func saveDiaryWith(title: String, body: String) {
         updateDiaryWith(title: title, body: body)
         
-        if CoreDataStack.shared.fetchDiary(with: diaryItem?.id) == nil {
+        if CoreDataManager.shared.fetchDiary(with: diaryItem?.id) == nil {
             generateDiary()
             
             if diaryItem?.title == Namespace.emptyString,
                diaryItem?.body == Namespace.emptyString { return }
             
-            CoreDataStack.shared.insertDiary(diaryItem)
-            let id = CoreDataStack.shared.fetchID(of: diaryItem)
+            CoreDataManager.shared.insertDiary(diaryItem)
+            let id = CoreDataManager.shared.fetchID(of: diaryItem)
             diaryItem?.id = id
         } else {
-            CoreDataStack.shared.updateDiary(diaryItem)
+            CoreDataManager.shared.updateDiary(diaryItem)
         }
     }
     
-    mutating func generateDiary() {
+    private func generateDiary() {
         diaryItem = DiaryModel(title: Namespace.emptyString,
                                body: Namespace.emptyString,
                                createdAt: Date())
     }
     
-    mutating func updateDiaryWith(title: String, body: String) {
+    private func updateDiaryWith(title: String, body: String) {
         diaryItem?.title = title
         diaryItem?.body = body
         diaryItem?.createdAt = Date()
     }
     
-    mutating func fetchDiary(data: DiaryModel) {
+    func fetchDiary(data: DiaryModel) {
         diaryItem = data
     }
 
@@ -59,8 +59,8 @@ struct DiaryItemManager {
         return form
     }
     
-    mutating func deleteDiary(data: DiaryModel?) {
-        CoreDataStack.shared.deleteDiary(with: data?.id)
+    func deleteDiary(data: DiaryModel?) {
+        CoreDataManager.shared.deleteDiary(with: data?.id)
         diaryItem = nil
     }
 }
