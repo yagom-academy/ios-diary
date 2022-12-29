7880f0633a49c1a65e34fa068b42052376b6c509
jsa0224 <voidness45@gmail.com>
2022-12-29 20:03:47 +0900

    feat: CoreDataManager의 오류를 전달해서 얼럿을 띄우는 AlertManager 구현

---
+++ Diary/Resource/Common/AlertManager.swift
@@ -0,0 +1,17 @@
+//
+//  AlertManager.swift
+//  Diary
+//
+//  Created by SummerCat and som on 2022/12/29.
+//
+
+import Foundation
+
+class AlertManager {
+    static let shared = AlertManager()
+    
+    func sendError(title: String) {
+        let notificationName = Notification.Name("CoreDataError")
+        NotificationCenter.default.post(name: notificationName, object: nil, userInfo: ["title": title])
+    }
+}
