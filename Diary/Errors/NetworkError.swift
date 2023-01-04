d000e77e4b036bf2d7a7e546c53f75537833693f
ash-youu <ash.you.dev@gmail.com>
2023-01-04 14:27:15 +0900

    feat: NetworkManager 타입 정의

---
+++ Diary/Errors/NetworkError.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkError.swift
+//  Diary
+//
+//  Created by 애종, 애쉬 on 2022/01/04.
+//
+
+enum NetworkError: Error {
+    case clientError
+    case missingData
+    case serverError
+}
---
+++ Diary/Model/NetworkManager.swift
@@ -0,0 +1,49 @@
+//
+//  HTTPManager.swift
+//  Diary
+//
+//  Created by 애종, 애쉬 on 2022/01/04.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func requestGet(url: String, completion: @escaping (Result<Data, NetworkError>) -> Void) {
+        guard let validURL = URL(string: url) else {
+            completion(.failure(.clientError))
+            return
+        }
+        
+        var urlRequest = URLRequest(url: validURL)
+        urlRequest.httpMethod = "GET"
+        
+        requestToServer(with: urlRequest, completion: completion)
+    }
+    
+    private func requestToServer(with urlRequest: URLRequest, completion: @escaping (Result<Data, NetworkError>) -> Void) {
+        URLSession.shared.dataTask(with: urlRequest) { data, urlResponse, error in
+            guard let data = data else {
+                completion(.failure(.clientError))
+                return
+            }
+            
+            guard let response = urlResponse as? HTTPURLResponse, (200..<300).contains(response.statusCode) else {
+                if let response = urlResponse as? HTTPURLResponse {
+                    print(response.statusCode)
+                }
+                return
+            }
+            
+            guard error == nil else {
+                completion(.failure(.serverError))
+                return
+            }
+            
+            completion(.success(data))
+        }.resume()
+    }
+}
