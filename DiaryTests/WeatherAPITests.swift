38a00fe41be34abe43c2fa7089cefc541e50fd4d
lxodud <xodud1998223@naver.com>
2023-01-04 15:00:45 +0900

    :test_tube: WeatherAPITests test 초기 설정

---
+++ DiaryTests/WeatherAPITests.swift
@@ -0,0 +1,21 @@
+//
+//  DiaryTests.swift
+//  DiaryTests
+//
+//  Created by 이태영 on 2023/01/04.
+//
+
+import XCTest
+@testable import Diary
+
+final class WeatherAPITests: XCTestCase {
+    var sut: NetworkManager!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+}
