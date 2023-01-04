f5adaf753432ccd572d1697255d1bd18a9914c66
DragonYG <kioswe92@gmail.com>
2023-01-04 15:06:14 +0900

    feat: WeatherAPI & Weather 모델 구현

---
+++ Diary/Source/Model/Weather.swift
@@ -0,0 +1,12 @@
+//
+//  Weather.swift
+//  Diary
+//  Created by inho, dragon on 2023/01/04.
+//
+
+import Foundation
+
+struct Weather: Decodable {
+    let main: String
+    let icon: String
+}
---
+++ Diary/Source/Model/WeatherAPI.swift
@@ -0,0 +1,11 @@
+//
+//  WeatherAPI.swift
+//  Diary
+//  Created by inho, dragon on 2023/01/04.
+//
+
+import Foundation
+
+struct WeatherAPI: Decodable {
+    let weather: [Weather]
+}
