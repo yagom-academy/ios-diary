ca7b7bae98ed698137485a3b457c2f2ffbd25f3c
sehong <tjsgh0801@naver.com>
2023-04-24 14:45:48 +0900

    feat: Json 데이터 모델 구현

---
+++ Diary/Model/Diary.swift
@@ -0,0 +1,16 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Seoyeon Hong on 2023/04/24.
+//
+
+import Foundation
+
+struct Diary: Decodable {
+    
+    let title: String
+    let body: String
+    let createdAt: Double
+    
+}
