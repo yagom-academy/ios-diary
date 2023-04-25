66eb962109839600b5a76be288680f454039fee7
minsu3007 <minsu3007@gmail.com>
2023-04-25 20:45:55 +0900

    feat: configureInitailView method

--- Diary/Controller/DetailViewController.swift
+++ Diary/Controller/DetailViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    private var textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
+    private var textView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textView
+    }()
     private var sampleData: DiaryModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        configureInitailView()
     }
     
     init(sampleData: DiaryModel? = nil) {
@@ -23,5 +36,20 @@ class DetailViewController: UIViewController {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureInitailView() {
+        guard let item = sampleData else {
+            let today = Date().timeIntervalSince1970
+            let todayText = Date.convertToDate(by: Int(today))
+            
+            self.navigationItem.title = todayText
+            
+            return
+        }
+        
+        self.navigationItem.title = Date.convertToDate(by: item.date)
+        self.textField.text = item.title
+        self.textView.text = item.body
     }
 }
