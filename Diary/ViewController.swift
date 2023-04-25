1ad107952bf6b3bb6569578d998f9bda08e9f54f
minsu3007 <minsu3007@gmail.com>
2023-04-25 13:14:03 +0900

    feat: TableView autolayout 함수 구현

--- Diary/ViewController.swift
+++ Diary/ViewController.swift
@@ -7,10 +7,31 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    
+    private var tableView: UITableView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        self.tableView = tableView
     }
 }
