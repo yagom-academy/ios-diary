5b39c584c2d30e9d398b623f5b78dce543f9e459
sehong <tjsgh0801@naver.com>
2023-04-26 13:49:57 +0900

    refactor: textField -> textView로 수정, 레이아웃 맞지 않는 부분 수정

--- Diary/Controller/DiaryDetailViewController.swift
+++ Diary/Controller/DiaryDetailViewController.swift
@@ -8,75 +8,69 @@
 import UIKit
 
 final class DiaryDetailViewController: UIViewController {
-    
+
     private let diaryItem: Diary
-    
-    private let diaryDetailStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 10
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return stackView
+
+    private let titleTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .preferredFont(forTextStyle: .subheadline)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textContainerInset = .zero
+        textView.isScrollEnabled = false
+        return textView
     }()
-    
-    private let titleTextField: UITextField = {
-        let textField = UITextField()
-        textField.font = .preferredFont(forTextStyle: .subheadline)
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        
-        return textField
-    }()
-    
+
     private let bodyTextView: UITextView = {
         let textView = UITextView()
         textView.font = .preferredFont(forTextStyle: .subheadline)
         textView.translatesAutoresizingMaskIntoConstraints = false
-        
+        textView.textContainerInset = .zero
         return textView
     }()
-    
+
     init(diaryItem: Diary) {
         self.diaryItem = diaryItem
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
         configureCellData()
         setupLayout()
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-          self.view.endEditing(true)
-      }
-    
+        self.view.endEditing(true)
+    }
+
     private func setupNavigationBar() {
         title = Formatter.changeToString(from: diaryItem.createdAt)
     }
-    
+
     private func configureCellData() {
-        titleTextField.text = diaryItem.title
+        titleTextView.text = diaryItem.title
         bodyTextView.text = diaryItem.body
     }
-    
+
     private func setupLayout() {
         view.backgroundColor = .systemBackground
-        view.addSubview(diaryDetailStackView)
-        
-        diaryDetailStackView.addArrangedSubview(titleTextField)
-        diaryDetailStackView.addArrangedSubview(bodyTextView)
+        view.addSubview(titleTextView)
+        view.addSubview(bodyTextView)
+
         NSLayoutConstraint.activate([
-            diaryDetailStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8.0),
-            diaryDetailStackView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
-            diaryDetailStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8.0),
-            diaryDetailStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8.0)
+            titleTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            titleTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            titleTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            bodyTextView.topAnchor.constraint(equalTo: titleTextView.bottomAnchor),
+            bodyTextView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
+            bodyTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            bodyTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8)
         ])
     }
-    
+
 }
