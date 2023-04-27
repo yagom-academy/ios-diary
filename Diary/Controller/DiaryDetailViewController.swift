30ffbad6cb773c7969ab3ed56b6d9bddff1c25bf
minsu3007 <minsu3007@gmail.com>
2023-04-27 15:59:33 +0900

    refactor: KeyboardLayout Notification -> Autolayout으로 수정

--- Diary/Controller/DiaryDetailViewController.swift
+++ Diary/Controller/DiaryDetailViewController.swift
@@ -21,7 +21,6 @@ final class DiaryDetailViewController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
-        setUpNotification()
     }
     
     init(diaryItem: Diary) {
@@ -31,17 +30,6 @@ final class DiaryDetailViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(
-            self,
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil)
-        NotificationCenter.default.removeObserver(
-            self,
-            name: UIResponder.keyboardWillHideNotification,
-            object: nil)
     }
     
     private func configureInitailView() {
@@ -54,37 +42,6 @@ final class DiaryDetailViewController: UIViewController {
         }
         
         self.diaryTextView.text = diaryItem.title + "\n\n" + diaryItem.body
-    }
-    
-    private func setUpNotification() {
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(showKeyboard),
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil
-        )
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(hideKeyboard),
-            name: UIResponder.keyboardWillHideNotification,
-            object: nil
-        )
-    }
-    
-    @objc private func showKeyboard(_ notification: Notification) {
-        guard let userInfo = notification.userInfo,
-              var keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
-        
-        keyboardFrame = view.convert(keyboardFrame, from: nil)
-        var contentInset = diaryTextView.contentInset
-        contentInset.bottom = keyboardFrame.size.height
-        diaryTextView.contentInset = contentInset
-        diaryTextView.scrollIndicatorInsets = diaryTextView.contentInset
-    }
-    
-    @objc private func hideKeyboard(_ notification: Notification) {
-        diaryTextView.contentInset = UIEdgeInsets.zero
-        diaryTextView.scrollIndicatorInsets = diaryTextView.contentInset
     }
     
     @objc private func doneButtonTapped(sender: Any) {
@@ -109,7 +66,7 @@ extension DiaryDetailViewController {
             diaryTextView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
             diaryTextView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
             diaryTextView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
-            diaryTextView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+            diaryTextView.bottomAnchor.constraint(equalTo: self.view.keyboardLayoutGuide.topAnchor)
         ])
     }
 }
