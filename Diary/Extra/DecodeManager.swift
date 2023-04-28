0ec413ed09961e1e3d84228fc1f63ffe63fbfd78
Jun <59365211+juun97@users.noreply.github.com>
2023-04-28 17:49:34 +0900

    refactor : error 타입 선언 위치 변경

--- Diary/Extra/DecodeManager.swift
+++ Diary/Extra/DecodeManager.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 final class DecodeManager {
+    private enum DecodeError: LocalizedError {
+        case invalidFileError
+        case decodingFailureError
+        
+        var errorDescription: String? {
+            switch self {
+            case .invalidFileError:
+                return "해당 file이 존재하지 않습니다."
+            case .decodingFailureError:
+                return "해당 file을 디코딩할 수 없습니다."
+            }
+        }
+    }
+    
     private let decoder = JSONDecoder()
     
     func decodeJSON<T: Decodable>(fileName: String, type: T.Type) throws -> T? {
