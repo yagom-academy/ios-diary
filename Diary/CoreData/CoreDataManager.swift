d3205fc17d342f551a98ecf0ee8a9810cc137bee
ROWAN <kimkj93@naver.com>
2023-05-01 15:35:17 +0900

    feat: DiaryCoreDataManager createDiary 메서드 정의

--- Diary/CoreData/CoreDataManager.swift
+++ Diary/CoreData/CoreDataManager.swift
@@ -8,22 +8,30 @@
 import Foundation
 import CoreData
 
-final class CoreDataManager {
-    static var shared: CoreDataManager = CoreDataManager()
+final class DiaryCoreDataManager {
+    static var shared: DiaryCoreDataManager = DiaryCoreDataManager()
 
-    lazy var persistentContainer: NSPersistentContainer = {
+    private lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Diary")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        
+        container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        
         return container
     }()
+    
+    private var context: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    private var diaryEntity: NSEntityDescription? {
+        NSEntityDescription.entity(forEntityName: "Diary", in: context)
+    }
 
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        
+    private func saveContext() {
         if context.hasChanges {
             do {
                 try context.save()
@@ -33,5 +41,16 @@ final class CoreDataManager {
             }
         }
     }
-
+    
+    func createDiary(title: String?, body: String?, date: String?) {
+        if let diaryEntity {
+            let diary = Diary(entity: diaryEntity, insertInto: context)
+            diary.setValue(title, forKey: "title")
+            diary.setValue(body, forKey: "body")
+            diary.setValue(date, forKey: "date")
+            diary.setValue(UUID(), forKey: "id")
+            
+            saveContext()
+        }
+    }
 }
