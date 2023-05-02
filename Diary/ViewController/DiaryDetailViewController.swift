d244edd71c16608e1993932f22cfb66db131bf45
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-05-02 11:41:38 +0900

    feat: textField 생성으로 detaiVC title, body 구분

--- Diary/ViewController/DiaryDetailViewController.swift
+++ Diary/ViewController/DiaryDetailViewController.swift
@@ -11,12 +11,30 @@ final class DiaryDetailViewController: UIViewController {
     private var diary: SampleDiary?
     private var isAutomaticKeyboard: Bool?
     
+    private lazy var diaryTitleField: UITextField = {
+        let titleField = UITextField()
+        if let diary = self.diary {
+            titleField.text = diary.title
+        }
+        titleField.attributedPlaceholder = NSAttributedString(string: "제목을 입력하세요", attributes: [NSAttributedString.Key.foregroundColor: UIColor.secondaryLabel])
+        titleField.addDoneButton(title: "Done", target: self, selector: #selector(dismissKeyboard))
+        
+        return titleField
+    }()
+    
     private lazy var diaryTextView: UITextView = {
         let textView = UITextView()
-        guard let diary = self.diary else { return textView }
-        textView.text = diary.title + "\n\n" + diary.body
+        if let diary = self.diary {
+            textView.text = diary.body
+        } else {
+            textView.text = "내용을 입력하세요"
+            textView.textColor = .secondaryLabel
+        }
+     
         textView.font = UIFont.preferredFont(forTextStyle: .body)
         textView.addDoneButton(title: "Done", target: self, selector: #selector(dismissKeyboard))
+        
+        textView.delegate = self
         
         return textView
     }()
@@ -36,13 +54,15 @@ final class DiaryDetailViewController: UIViewController {
         view.backgroundColor = .white
         
         configureNavigationBar()
-        configureDiaryTextView()
+        configureDiaryView()
         setUpNotification()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         if isAutomaticKeyboard == true {
-            diaryTextView.becomeFirstResponder()
+            diaryTextView.addDoneButton(title: "Done", target: self, selector: #selector(dismissKeyboard))
+            diaryTitleField.addDoneButton(title: "Done", target: self, selector: #selector(dismissKeyboard))
+            diaryTitleField.becomeFirstResponder()
         }
     }
     
@@ -53,21 +73,31 @@ final class DiaryDetailViewController: UIViewController {
         self.navigationItem.title = DateFormatterManager.shared.convertToFomattedDate(of: today)
     }
     
-    private func configureDiaryTextView() {
+    private func configureDiaryView() {
+        view.addSubview(diaryTitleField)
         view.addSubview(diaryTextView)
+        
         let safeArea = view.safeAreaLayoutGuide
         
         diaryTextView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
-        let heightConstraint = diaryTextView.heightAnchor.constraint(equalTo: safeArea.heightAnchor)
-        heightConstraint.priority = .defaultHigh
+        let textViewHeight = diaryTextView.heightAnchor.constraint(equalTo: safeArea.heightAnchor)
+        let textFieldHeight = diaryTitleField.heightAnchor.constraint(equalTo: safeArea.heightAnchor)
+        textViewHeight.priority = .defaultHigh
+        textFieldHeight.priority = .defaultLow
         
         diaryTextView.translatesAutoresizingMaskIntoConstraints = false
+        diaryTitleField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            diaryTextView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
-            diaryTextView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 14),
-            diaryTextView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -14),
+            diaryTitleField.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
+            diaryTitleField.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 14),
+            diaryTitleField.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -14),
+            textFieldHeight,
+            
+            diaryTextView.topAnchor.constraint(equalTo: diaryTitleField.bottomAnchor, constant: 20),
+            diaryTextView.leadingAnchor.constraint(equalTo: diaryTitleField.leadingAnchor),
+            diaryTextView.trailingAnchor.constraint(equalTo: diaryTitleField.trailingAnchor),
             diaryTextView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
-            heightConstraint
+            textViewHeight
         ])
     }
     
@@ -97,7 +127,7 @@ final class DiaryDetailViewController: UIViewController {
         diaryTextView.contentInset = contentInset
         diaryTextView.scrollIndicatorInsets = diaryTextView.contentInset
     }
-    
+
     @objc internal func keyboardWillHide(_ notification: Notification) {
         diaryTextView.contentInset = UIEdgeInsets.zero
         diaryTextView.scrollIndicatorInsets = diaryTextView.contentInset
@@ -105,5 +135,20 @@ final class DiaryDetailViewController: UIViewController {
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+}
+
+extension DiaryDetailViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        guard diaryTextView.textColor == .secondaryLabel else { return }
+        diaryTextView.textColor = .label
+        diaryTextView.text = nil
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if diaryTextView.text.isEmpty {
+            diaryTextView.text = "내용을 입력하세요"
+            diaryTextView.textColor = .secondaryLabel
+        }
     }
 }
