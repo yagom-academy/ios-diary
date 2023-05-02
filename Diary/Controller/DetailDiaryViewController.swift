91f6073837c8a64efd252b3ae545cb64c982385c
parkmuri <smxhmu@naver.com>
2023-05-02 17:16:58 +0900

    refactor: 메서드 순서 변경 및 NotificationObserver추가

--- Diary/Controller/DetailDiaryViewController.swift
+++ Diary/Controller/DetailDiaryViewController.swift
@@ -45,8 +45,13 @@ final class DetailDiaryViewController: UIViewController {
         configureUI()
         configureSubview()
         configureConstraint()
-        addKeyboardNotification()
         configureKeyboard()
+        addKeyboardNotification()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        saveDiary()
     }
     
     func configureContent(diary: Diary) {
@@ -55,6 +60,138 @@ final class DetailDiaryViewController: UIViewController {
         diaryTextView.contentOffset = CGPoint.zero
         diaryDate = Date(timeIntervalSince1970: diary.date).convertDate()
         self.title = diaryDate
+    }
+    
+    // MARK: - configure method
+    private func configureSubview() {
+        view.addSubview(diaryTextView)
+    }
+    
+    private func configureConstraint() {
+        bottomConstraint = diaryTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        bottomConstraint?.isActive = true
+        
+        NSLayoutConstraint.activate([
+            diaryTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            diaryTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            diaryTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        diaryTextView.setContentOffset(.zero, animated: true)
+        detailButton.addTarget(self, action: #selector(showDetailAction), for: .touchUpInside)
+        let detailDiaryButon = UIBarButtonItem(customView: detailButton)
+        navigationItem.rightBarButtonItem = detailDiaryButon
+        
+        if diaryDate == nil {
+            title = Date().convertDate()
+        }
+    }
+    
+    private func configureKeyboard() {
+        if isCreateDiary {
+            diaryTextView.becomeFirstResponder()
+        }
+    }
+    
+    private func configureDiary() -> Diary? {
+        let id = UUID()
+        let diaryContents = diaryTextView.text.split(separator: "\n", maxSplits: 1)
+        let title = String(diaryContents[0])
+        var body: String
+        
+        if diaryContents.count == 2 {
+            body = String(diaryContents[1])
+        } else {
+            body = ""
+        }
+        
+        guard let date = Date().timeIntervalSince1970.roundDownNumber() else { return nil }
+        
+        return Diary(title: title, body: body, date: date, id: id)
+    }
+    
+    // MARK: - Notification method
+    private func addKeyboardNotification() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidHide),
+                                               name: UIResponder.keyboardDidHideNotification,
+                                               object: nil)
+    }
+    
+    @objc
+    private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardFrame: NSValue =
+            notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+           let firstWindow = UIApplication.shared.windows.first {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            let changedHeight = -(keyboardHeight - firstWindow.safeAreaInsets.bottom)
+            bottomConstraint?.constant = changedHeight
+        }
+    }
+    
+    @objc
+    private func keyboardWillHide(notification: NSNotification) {
+        bottomConstraint?.constant = 0
+    }
+    
+    @objc
+    private func keyboardDidHide(notification: NSNotification) {
+//        self.navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - Action Sheet method
+    @objc
+    private func showDetailAction() {
+        let actionSheet = UIAlertController(title: nil,
+                                            message: nil,
+                                            preferredStyle: .actionSheet)
+        
+        let share = UIAlertAction(title: "공유", style: .default) { _ in
+            self.showActivityVC()
+        }
+        
+        let delete = UIAlertAction(title: "삭제", style: .destructive) { _ in
+            
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel) { _ in
+        }
+        
+        actionSheet.addAction(share)
+        actionSheet.addAction(delete)
+        actionSheet.addAction(cancel)
+        
+        self.present(actionSheet, animated: true)
+    }
+    
+    private func showActivityVC() {
+        let activityItems = [UIActivity.ActivityType.airDrop, .message, .mail]
+        let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        
+        activityVC.popoverPresentationController?.sourceView = self.view
+        
+        self.present(activityVC, animated: true, completion: nil)
+    }
+    
+    // MARK: - CoreData method
+    private func saveDiary() {
+        if isCreateDiary {
+            createDiary()
+        } else {
+            updateDiary()
+        }
     }
     
     private func createDiary() {
@@ -74,131 +211,6 @@ final class DetailDiaryViewController: UIViewController {
         let updatedDiary = Diary(title: title, body: body, date: date, id: id)
         
         coreDataManager.updateDiary(diary: updatedDiary)
-    }
-    
-    private func configureDiary() -> Diary? {
-        let diaryContents = diaryTextView.text.split(separator: "\n", maxSplits: 1)
-        let title = String(diaryContents[0])
-        let body = String(diaryContents[1])
-        let id = UUID()
-        
-        guard let date = Date().timeIntervalSince1970.roundDownNumber() else { return nil }
-        
-        return Diary(title: title, body: body, date: date, id: id)
-    }
-    
-    private func configureUI() {
-        view.backgroundColor = .white
-        diaryTextView.setContentOffset(.zero, animated: true)
-        detailButton.addTarget(self, action: #selector(showDetailAction), for: .touchUpInside)
-        let detailDiaryButon = UIBarButtonItem(customView: detailButton)
-        navigationItem.rightBarButtonItem = detailDiaryButon
-        
-        if diaryDate == nil {
-            title = Date().convertDate()
-        }
-    }
-    
-    @objc
-    private func showDetailAction() {
-        let actionSheet = UIAlertController(title: nil,
-                                            message: nil,
-                                            preferredStyle: .actionSheet)
-        
-        let save = UIAlertAction(title: "저장", style: .default) { _ in
-            self.diaryTextView.endEditing(true)
-            if self.isCreateDiary {
-                self.createDiary()
-            } else {
-                self.updateDiary()
-            }
-        }
-        
-        let share = UIAlertAction(title: "공유", style: .default) { _ in
-            self.showActivityVC()
-        }
-        
-        let delete = UIAlertAction(title: "삭제", style: .destructive) { _ in
-            
-        }
-        
-        let cancel = UIAlertAction(title: "취소", style: .cancel) { _ in
-            self.dismiss(animated: true)
-        }
-        
-        actionSheet.addAction(save)
-        actionSheet.addAction(share)
-        actionSheet.addAction(delete)
-        actionSheet.addAction(cancel)
-        
-        self.present(actionSheet, animated: true)
-    }
-    
-    private func showActivityVC() {
-        let activityItems = [UIActivity.ActivityType.airDrop, .message, .mail]
-        let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
-        
-        activityVC.popoverPresentationController?.sourceView = self.view
-        
-        self.present(activityVC, animated: true, completion: nil)
-    }
-    
-    private func configureKeyboard() {
-        if isCreateDiary {
-            diaryTextView.becomeFirstResponder()
-        }
-    }
-    
-    private func configureSubview() {
-        view.addSubview(diaryTextView)
-    }
-    
-    private func configureConstraint() {
-        bottomConstraint = diaryTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        bottomConstraint?.isActive = true
-        
-        NSLayoutConstraint.activate([
-            diaryTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            diaryTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            diaryTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
-        ])
-    }
-    
-    private func addKeyboardNotification() {
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillShow),
-                                               name: UIResponder.keyboardWillShowNotification,
-                                               object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillHide),
-                                               name: UIResponder.keyboardWillHideNotification,
-                                               object: nil)
-    }
-    
-    private func removeKeyboardNotification() {
-        NotificationCenter.default.removeObserver(self,
-                                                  name: UIResponder.keyboardWillShowNotification,
-                                                  object: nil)
-        NotificationCenter.default.removeObserver(self,
-                                                  name: UIResponder.keyboardWillHideNotification,
-                                                  object: nil)
-    }
-    
-    @objc
-    private func keyboardWillShow(notification: NSNotification) {
-        if let keyboardFrame: NSValue =
-            notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
-           let firstWindow = UIApplication.shared.windows.first {
-            let keyboardRectangle = keyboardFrame.cgRectValue
-            let keyboardHeight = keyboardRectangle.height
-            let changedHeight = -(keyboardHeight - firstWindow.safeAreaInsets.bottom)
-            bottomConstraint?.constant = changedHeight
-        }
-    }
-    
-    @objc
-    private func keyboardWillHide(notification: NSNotification) {
-        bottomConstraint?.constant = 0
     }
 }
 
