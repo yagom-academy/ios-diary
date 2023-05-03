fd134b32b00ec63332d51d7e1871f5ae2346035c
HarryHyeon <hharry0728@gmail.com>
2023-05-03 19:06:41 +0900

    feat: DiaryListViewController에서 cell을 스와이프할때 삭제 기능 추가

--- Diary/Controller/DiaryListViewController.swift
+++ Diary/Controller/DiaryListViewController.swift
@@ -10,6 +10,8 @@ final class DiaryListViewController: UIViewController {
     private let tableView = UITableView()
     private var diaryList: [Diary] = []
     private let sampleDecoder = DiaryDecodeManager()
+    private let alertFactory: AlertFactoryService = AlertMaker()
+    private let alertDataMaker: AlertDataService = AlertViewDataMaker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -112,20 +114,48 @@ extension DiaryListViewController: UITableViewDelegate {
         _ tableView: UITableView,
         trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath
     ) -> UISwipeActionsConfiguration? {
-        let share = UIContextualAction(style: .normal, title: nil) { _, _, completion in
+        let share = UIContextualAction(style: .normal, title: nil) {
+            [weak self] _, _, completion in
 //            self.showActivityView()
             completion(true)
         }
         share.image = UIImage(systemName: "square.and.arrow.up")
         share.backgroundColor = .systemGreen
         
-        let delete = UIContextualAction(style: .destructive, title: nil) { _, _, completion in
-//            self.showDeleteAlert()
+        let delete = UIContextualAction(style: .destructive, title: nil) {
+            [weak self] _, _, completion in
+            self?.presentDeleteAlert(indexPath: indexPath)
             completion(true)
         }
         delete.image = UIImage(systemName: "trash")
         delete.backgroundColor = .systemRed
         
         return UISwipeActionsConfiguration(actions: [delete, share])
+    }
+}
+
+// MARK: - Alert
+extension DiaryListViewController {
+    private func presentDeleteAlert(indexPath: IndexPath) {
+        let delete = AlertActionData(actionTitle: "삭제",
+                                     actionStyle: .destructive) { [weak self] in
+            guard let id = self?.diaryList[indexPath.row].id else { return }
+            
+            self?.diaryList.remove(at: indexPath.row)
+            DiaryCoreDataManager.shared.deleteDiary(id: id)
+            self?.tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        let cancel = AlertActionData(actionTitle: "취소",
+                                     actionStyle: .cancel,
+                                     completion: nil)
+        let actionDataList = [delete, cancel]
+        let alertData = alertDataMaker.makeData(title: "진짜요?",
+                                                message: "정말로 삭제하시겠어요?",
+                                                alertStyle: .alert,
+                                                actionDataList: actionDataList)
+        let alertController = alertFactory.make(for: alertData)
+        
+        present(alertController, animated: true)
     }
 }
