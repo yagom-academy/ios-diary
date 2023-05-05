7f3487e6f50b9c27900be7badc93bdf8ea86b757
ROWAN <kimkj93@naver.com>
2023-05-05 23:24:01 +0900

    refactor: DiaryCoreDataManager 역할 분리
    추상화에 용이하도록 타입 분리
    CoreDataStack 관리 타입: CoreDataManager
    Model 객체를 storage에 CRUD하는 타입: DiaryDataManager

---
+++ Diary/CoreData/CoreDataManager.swift
@@ -0,0 +1,52 @@
+//
+//  CoreDataManager.swift
+//  Diary
+//
+//  Created by Rowan, Harry on 2023/05/05.
+//
+
+import Foundation
+import CoreData
+
+final class CoreDataManager {
+    static let shared = CoreDataManager()
+    
+    private init() { }
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "DiaryDAO")
+        
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    func fetch<T: NSManagedObject>(request: NSFetchRequest<T>) -> [T] {
+        do {
+            let fetchResult = try self.context.fetch(request)
+            return fetchResult
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+    
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
--- Diary/CoreData/DiaryCoreDataManager.swift
+++ Diary/CoreData/DiaryCoreDataManager.swift
@@ -8,79 +8,49 @@
 import Foundation
 import CoreData
 
-final class DiaryCoreDataManager {
-    static var shared: DiaryCoreDataManager = DiaryCoreDataManager()
-
-    private lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "DiaryDAO")
-        
-        container.loadPersistentStores(completionHandler: { (_, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        
-        return container
-    }()
+struct DiaryDataManager {
+    let storage = CoreDataManager.shared
     
-    private var context: NSManagedObjectContext {
-        persistentContainer.viewContext
+    var diaryEntity: NSEntityDescription? {
+        return NSEntityDescription.entity(forEntityName: "DiaryDAO",
+                                          in: storage.context)
     }
     
-    private var diaryEntity: NSEntityDescription? {
-        NSEntityDescription.entity(forEntityName: "DiaryDAO", in: context)
-    }
-
-    private func saveContext() {
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    
-    func createDiary(title: String, body: String, date: Double, id: UUID) -> DiaryDAO? {
+    func createDAO(from data: Diary) {
         if let diaryEntity {
-            let diary = DiaryDAO(entity: diaryEntity, insertInto: context)
-            diary.setValue(title, forKey: "title")
-            diary.setValue(date, forKey: "date")
-            diary.setValue(body, forKey: "body")
-            diary.setValue(id, forKey: "id")
+            let diary = DiaryDAO(entity: diaryEntity, insertInto: storage.context)
+            diary.setValue(data.title, forKey: "title")
+            diary.setValue(data.updatedDate, forKey: "date")
+            diary.setValue(data.body, forKey: "body")
+            diary.setValue(data.id, forKey: "id")
             
             saveContext()
-            
-            return diary
         }
-        
-        return nil
     }
     
-    func fetchDiary() -> Result<[DiaryDAO], Error> {
+    func readAllDAO() -> [DiaryDAO] {
         let request = DiaryDAO.fetchRequest()
         let sortDescriptor = NSSortDescriptor(key: "date", ascending: false)
         
         request.sortDescriptors = [sortDescriptor]
         
-        let fetchResult = Result { try self.context.fetch(request) }
+        let fetchResult = storage.fetch(request: request)
         
         return fetchResult
     }
     
-    func updateDiary(title: String?, body: String?, date: Double, id: UUID) {
+    func updateDAO(data: Diary) {
         let request = DiaryDAO.fetchRequest()
-        let predicate = NSPredicate(format: "id == %@", id.uuidString)
+        let predicate = NSPredicate(format: "id == %@", data.id.uuidString)
         request.predicate = predicate
         
-        let fetchResult = try? self.context.fetch(request)
+        let fetchResult = storage.fetch(request: request)
         
-        guard let diary = fetchResult?.first else { return }
+        guard let diary = fetchResult.first else { return }
         
-        diary.setValue(title, forKey: "title")
-        diary.setValue(date, forKey: "date")
-        diary.setValue(body, forKey: "body")
+        diary.setValue(data.title, forKey: "title")
+        diary.setValue(data.updatedDate, forKey: "date")
+        diary.setValue(data.body, forKey: "body")
         
         saveContext()
     }
@@ -96,5 +66,16 @@ final class DiaryCoreDataManager {
         
         context.delete(diary)
         saveContext()
+    }
+
+    private func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 }
