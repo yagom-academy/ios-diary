f214fdcc50fba10e700c0b21cfda62c030ab9a69
Andrew-0411 <whk1140@naver.com>
2023-05-10 12:48:55 +0900

    feat: APIEndpoint 프로토콜 생성

--- Diary/Model/Network/APIEndpoint.swift
+++ Diary/Model/Network/APIEndpoint.swift
@@ -8,5 +8,50 @@
 import Foundation
 
 protocol APIEndpoint {
+    associatedtype APIResponse: Decodable
     
+    var decoder: DecodingUtility { get }
+    var method: HTTPMethod { get }
+    var baseURLLiteral: String { get }
+    var path: String { get }
+    var url: URL? { get }
+    var headers: [String: String]? { get }
+    var queries: [String: String?] { get }
+}
+
+extension APIEndpoint {
+    var decoder: DecodingUtility {
+        return JSONDecodingUtility()
+    }
+    
+    var url: URL? {
+        guard let url = URL(string: baseURLLiteral)?.appendingPathComponent(path) else {
+            return nil
+        }
+        
+        var urlComponents = URLComponents(string: url.absoluteString)
+        let quries = self.queries.map { key, value in
+            URLQueryItem(name: key, value: value)
+        }
+        
+        urlComponents?.queryItems = quries
+        return urlComponents?.url
+    }
+    
+    var urlRequest: URLRequest? {
+        guard let url = self.url else {
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = self.method.rawValue
+        
+        if let headers = self.headers {
+            headers.forEach { key, value in
+                request.addValue(value, forHTTPHeaderField: key)
+            }
+        }
+        
+        return request
+    }
 }
