16840a8fb74e2c7e087550d75ba17d48b294e24d
seunghyunCheon <csh7215@naver.com>
2023-05-10 21:17:32 +0900

    feat: WeatherResponse생성

---
+++ Diary/Model/Service/Weather/Response/WeatherResponse.swift
@@ -0,0 +1,62 @@
+//
+//  WeatherResponse.swift
+//  Diary
+//
+//  Created by Andrew, brody on 2023/05/10.
+//
+
+import Foundation
+
+struct WeatherResponse: Decodable {
+    let coord: Coord
+    let weather: [Weather]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+}
+
+struct Clouds: Decodable {
+    let all: Int
+}
+
+struct Coord: Decodable {
+    let lon, lat: Double
+}
+
+struct Main: Decodable {
+    let temp, feelsLike, tempMin, tempMax: Double
+    let pressure, humidity, seaLevel, grndLevel: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+    }
+}
+
+struct Sys: Decodable {
+    let country: String
+    let sunrise, sunset: Int
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main, description, icon: String
+}
+
+struct Wind: Decodable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
---
+++ Diary/Model/Service/Weather/WeatherService.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherService.swift
+//  Diary
+//
+//  Created by Andrew, brody on 2023/05/10.
+//
+
+//import Foundation
+//
+//protocol WeatherService {
+//    var networkProvider: NetworkProvider { get }
+//
+//    @discardableResult
+//    func fetchWeatherInformation(
+//        latitude: Double,
+//        longtitude: Double,
+//        completion: @escaping ((Result<Bool, NetworkProvider>) -> Void)
+//    )
+//}
