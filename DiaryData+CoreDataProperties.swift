e40534a280c7d0b70ef85021993dd39af1de06d1
vetto <gzzjk159@naver.com>
2023-05-10 15:33:14 +0900

    feat: coredata migration 구현

--- DiaryData+CoreDataProperties.swift
+++ DiaryData+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  DiaryData+CoreDataProperties.swift
 //  Diary
 //
-//  Created by Christy, vetto on 2023/05/01.
+//  Created by Christy, vetto on 2023/05/10.
 //
 
 import Foundation
@@ -12,11 +12,13 @@ extension DiaryData {
     @nonobjc public class func fetchRequest() -> NSFetchRequest<DiaryData> {
         return NSFetchRequest<DiaryData>(entityName: "DiaryData")
     }
-
+    
     @NSManaged public var id: UUID?
     @NSManaged public var title: String?
     @NSManaged public var body: String?
     @NSManaged public var createDate: Double
+    @NSManaged public var weatherIcon: String?
+    @NSManaged public var weatherMain: String?
 }
 
 extension DiaryData: Identifiable {}
