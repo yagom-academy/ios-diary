b81bf7e17a47584f08416ab8555a14f78ce398fc
redmango1446@gmail.com <redmango1446>
2023-08-28 21:24:05 +0900

    feat: Diary DTO 추가 및 샘플Json파일 추가

---
+++ Diary/Model/DTO/Diary.swift
@@ -0,0 +1,21 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by redmango1446 on 2023/08/28.
+//
+
+struct DiaryList: Decodable {
+    let diary: [Diary]
+}
+
+struct Diary: Decodable {
+    let title: String
+    let body: String
+    let createdDate: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case title, body
+        case createdDate = "created_at"
+    }
+}
