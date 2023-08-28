ad9d8129daad66f9f2a52c6c5f839dcab3825bab
bubblecocoa <bubblecocoa@gmail.com>
2023-08-28 23:06:36 +0900

    refactor: 요구사항에 맞게 navigationItem 추가, 채택한 프로토콜을 extension으로 분리하여 가독성 향상

--- Diary/ViewController/DiaryListViewController.swift
+++ Diary/ViewController/DiaryListViewController.swift
@@ -6,20 +6,51 @@
 
 import UIKit
 
-class DiaryListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    let tableView = UITableView()
+final class DiaryListViewController: UIViewController {
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        view.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        configureNavigation()
+        configureBackgroundColor()
         configureTableView()
+    }
+    
+    private func configureNavigation() {
+        navigationItem.title = "일기장"
+        navigationItem.rightBarButtonItem = .init(systemItem: .add)
+    }
+    
+    private func configureBackgroundColor() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        configureTableViewConstraint()
         tableView.register(DiaryTableViewCell.self, forCellReuseIdentifier: DiaryTableViewCell.identifier)
         tableView.dataSource = self
         tableView.delegate = self
     }
     
+    private func configureTableViewConstraint() {
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension DiaryListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
@@ -30,16 +61,15 @@ class DiaryListViewController: UIViewController, UITableViewDelegate, UITableVie
         }
         
         cell.configureCell()
+        cell.accessoryType = .disclosureIndicator
         
         return cell
     }
-    
-    func configureTableView() {
-        NSLayoutConstraint.activate([
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
+}
+
+// MARK: - UITableViewDelegate
+extension DiaryListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
