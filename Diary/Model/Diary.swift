578a78bf010753b08b0c99ef7b9250692b19765a
DasanKim <tempzipp@gmail.com>
2023-08-28 22:13:04 +0900

    ✨feat: Diary DTO 생성

---
+++ Diary/Model/Diary.swift
@@ -0,0 +1,18 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Dasan, kyungmin on 2023/08/28.
+//
+
+struct Diary: Decodable {
+    let title: String
+    let body: String
+    let createdDate: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case body
+        case createdDate = "created_at"
+    }
+}
