e5eadee81b097655623dee6b95a1e8fdfaa15e3a
Whales <Whales.w.Jin@gmail.com>
2023-08-28 21:48:22 +0900

    ✨ Feat: listCollectionView 구현

--- Diary/Controller/DiaryViewController.swift
+++ Diary/Controller/DiaryViewController.swift
@@ -7,11 +7,29 @@
 import UIKit
 
 class DiaryViewController: UIViewController {
+    private var listCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureCollectionView()
+        configureView()
+    }
+    
+    private func configureView() {
         view.backgroundColor = .systemBackground
+        view.addSubview(listCollectionView)
+    }
+    
+    private func configureCollectionView() {
+        listCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createListLayout())
+        listCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+    
+    private func createListLayout() -> UICollectionViewLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        
+        return UICollectionViewCompositionalLayout.list(using: configuration)
     }
 }
 
