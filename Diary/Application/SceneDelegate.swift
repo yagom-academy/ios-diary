1a94e4c2acf4c1b8d576a87fcfa2c6c73a9120bc
h-suo <phstoo1021@naver.com>
2023-08-28 18:52:29 +0900

    🩹Chore: DiaryListViewController로 리네임

--- Diary/Application/SceneDelegate.swift
+++ Diary/Application/SceneDelegate.swift
@@ -10,14 +10,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else {
             return
         }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = UINavigationController(rootViewController: DiaryListViewController())
         window?.makeKeyAndVisible()
     }
 
