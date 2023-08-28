d77ee02a6520496b7edd8a527505eb74544508ab
mint3382 <mskim0926@naver.com>
2023-08-28 21:59:40 +0900

    feat: DiaryListViewController에 리스트를 보여줄 tableView 생성

--- Diary/ViewController/DiaryListViewController.swift
+++ Diary/ViewController/DiaryListViewController.swift
@@ -6,9 +6,40 @@
 
 import UIKit
 
-class DiaryListViewController: UIViewController {
+class DiaryListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        configureTableView()
+        tableView.register(DiaryTableViewCell.self, forCellReuseIdentifier: DiaryTableViewCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: DiaryTableViewCell.identifier, for: indexPath) as? DiaryTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.configureCell()
+        
+        return cell
+    }
+    
+    func configureTableView() {
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
