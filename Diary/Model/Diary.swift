f0d0d8ac3ecb41c52fb3870e5d06ea7be0f3bc78
mint3382 <mskim0926@naver.com>
2023-08-29 15:20:56 +0900

    feat: Diary 구조체 추가

---
+++ Diary/Model/Diary.swift
@@ -0,0 +1,20 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by MINT, BMO on 2023/08/28.
+//
+
+import Foundation
+
+struct Diary {
+    let title: String
+    let body: String
+    let date: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case body
+        case date = "created_at"
+    }
+}
