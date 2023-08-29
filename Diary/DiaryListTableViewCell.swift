03b134c69e7ed06844c77ce902bcf966f9453edc
hemg2 <hemg2@naver.com>
2023-08-29 11:15:40 +0900

    feat: tabelViewCell 생성

--- Diary/DiaryListTableViewCell.swift
+++ Diary/DiaryListTableViewCell.swift
@@ -2,22 +2,70 @@
 //  DiaryListTableViewCell.swift
 //  Diary
 //
-//  Created by 1 on 2023/08/29.
+//  Created by by Maxhyunm, Hamg on 2023/08/29.
 //
 
 import UIKit
 
 class DiaryListTableViewCell: UITableViewCell {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .body)
+        
+        return label
+    }()
+    
+    private let bodyLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .callout)
+        
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .caption1)
+        
+        return label
+    }()
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        setUpLabel()
+        configureUI()
     }
-
+    
+    private func setUpLabel() {
+        addSubview(titleLabel)
+        addSubview(dateLabel)
+        addSubview(bodyLabel)
+    }
+    
+    private func configureUI() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            dateLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            dateLabel.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            bodyLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            bodyLabel.leadingAnchor.constraint(equalTo: dateLabel.trailingAnchor, constant: 4),
+            bodyLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            bodyLabel.centerYAnchor.constraint(equalTo: dateLabel.centerYAnchor)
+        ])
+    }
 }
