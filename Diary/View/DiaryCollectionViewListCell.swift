09099f003894f44ef029a51c7a0a0b3d7fdb7da9
YaRkyungmin <kkgm94@naver.com>
2023-08-29 11:14:42 +0900

    ✨feat: DiaryCollectionViewListCell 생성

---
+++ Diary/View/DiaryCollectionViewListCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCollectionViewListCell.swift
+//  Diary
+//
+//  Created by Dasan, kyungmin on 2023/08/28.
+//
+
+import UIKit
+
+class DiaryCollectionViewListCell: UICollectionViewListCell {
+    
+}
