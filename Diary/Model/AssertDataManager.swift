4c5d02e74649f2e1f979413166b8b8f7012e92fa
DasanKim <tempzipp@gmail.com>
2023-08-29 21:48:38 +0900

    ✨feat: AssertDataManager 생성

---
+++ Diary/Model/AssertDataManager.swift
@@ -0,0 +1,22 @@
+//
+//  AssertDataManager.swift
+//  Diary
+//
+//  Created by Dasan, kyungmin on 2023/08/29.
+//
+
+import UIKit
+
+struct AssertDataManager {
+    func fetchDiaryData() throws -> [DiaryData] {
+        guard let dataAsset = NSDataAsset(name: "sample") else {
+            throw DataError.notFoundAsset
+        }
+        
+        guard let diaryList = try? JSONDecoder().decode([DiaryData].self, from: dataAsset.data) else {
+            throw DataError.failedDecoding
+        }
+        
+        return diaryList
+    }
+}
