14f4ea4041a39199afcddb33bd3b74e16b8a58e7
Hoon94 <dleognsdl1@naver.com>
2023-08-29 23:11:22 +0900

    ✴️feat: tableView 구현

---
+++ Diary/Controller/DiaryViewController.swift
@@ -0,0 +1,62 @@
+//
+//  Diary - DiaryViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class DiaryViewController: UIViewController {
+    private var tableView: UITableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+}
+
+extension DiaryViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            return UITableViewCell()
+        }
+
+        return cell
+    }
+}
+
+private extension DiaryViewController {
+    func configure() {
+        configureTableView()
+        configureCell()
+        configureSubviews()
+        configureConstraints()
+    }
+    
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func configureCell() {
+        tableView.register(DiaryCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func configureSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    func configureConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
---
+++ Diary/View/DiaryCell.swift
@@ -0,0 +1,21 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by hoon, karen on 2023/08/29.
+//
+
+import UIKit
+
+final class DiaryCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
