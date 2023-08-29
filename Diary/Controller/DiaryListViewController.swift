93d6c80ea0829b871c4d44e9c5022ae51a93c213
idinaloq <idinaloq@icloud.com>
2023-08-29 10:44:12 +0900

    feat: DiaryCollectionViewListCell 오토레이아웃 구현
    ViewController.swift -> DiaryListViewController.swift 네이밍 변경

---
+++ Diary/Controller/DiaryListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  Diary - DiaryListViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class DiaryListViewController: UIViewController {
+    private let collectionView: UICollectionView = {
+        let configuration: UICollectionLayoutListConfiguration = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(DiaryCollectionViewListCell.self, forCellWithReuseIdentifier: DiaryCollectionViewListCell.identifier)
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        configureUI()
+        configureAutoLayout()
+    }
+    private func configureUI() {
+        view.addSubview(collectionView)
+        view.backgroundColor = .systemBackground
+    }
+    private func configureAutoLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+}
+
+extension DiaryListViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DiaryCollectionViewListCell.identifier, for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
