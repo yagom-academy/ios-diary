178c28a5e8d85dcd327d4b6465cda1b9884a2f86
YaRkyungmin <kkgm94@naver.com>
2023-08-29 11:16:29 +0900

    ✨feat: DiaryViewController 메서드 및 Section 타입 구현
    setup Components
    configure UI
    setup Constraints
    listLayout
    Section

--- Diary/Controller/DiaryViewController.swift
+++ Diary/Controller/DiaryViewController.swift
@@ -8,8 +8,80 @@
 import UIKit
 
 final class DiaryViewController: UIViewController {
+    private var collectionView: UICollectionView!
+    private var diaryDataSource: UICollectionViewDiffableDataSource<Section, Diary>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupComponents()
+        configureUI()
+        setupConstraint()
+    }
+}
+
+// MARK: Setup Components
+extension DiaryViewController {
+    private func setupComponents() {
+        setupView()
+        setupCollectionView()
+    }
+    
+    private func setupView() {
         view.backgroundColor = .white
+    }
+    
+    private func setupCollectionView() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: listLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+}
+
+// MARK: Configure UI
+extension DiaryViewController {
+    private func configureUI() {
+        configureView()
+    }
+    
+    private func configureView() {
+        view.addSubview(collectionView)
+    }
+}
+
+// MARK: Setup Constraints
+extension DiaryViewController {
+    private func setupConstraint() {
+        setupCollectionViewConstraint()
+    }
+    
+    private func setupCollectionViewConstraint() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ])
+    }
+}
+
+// MARK: CollectionView DataSource
+extension DiaryViewController {
+    
+}
+
+// MARK: CollectionVeiw Layout
+extension DiaryViewController {
+    private func listLayout() -> UICollectionViewLayout {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        
+        return UICollectionViewCompositionalLayout.list(using: configuration)
+    }
+}
+
+// MARK: CollectionView Section
+extension DiaryViewController {
+    private enum Section {
+        case main
     }
 }
