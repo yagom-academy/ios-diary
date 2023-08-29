1b91d6f6d9b141382cfae1774ecb8b321fdf5fbf
JusBug <pksgwo123@gmail.com>
2023-08-29 15:25:06 +0900

    feat: JSON파일을 Sample 타입으로 디코딩하는 decodeJSON() 구현

--- Diary/ViewController/DiaryTableViewCell.swift
+++ Diary/ViewController/DiaryTableViewCell.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class DiaryTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var createdDateLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        titleLabel.text = nil
+        createdDateLabel.text = nil
+        bodyLabel.text = nil
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -20,4 +31,9 @@ class DiaryTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func configureLabel(sample: Sample) {
+        titleLabel.text = sample.title
+        bodyLabel.text = sample.body
+        createdDateLabel.text = String(sample.createdDate)
+    }
 }
--- Diary/ViewController/MainViewController.swift
+++ Diary/ViewController/MainViewController.swift
@@ -6,14 +6,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    var sample: [Sample] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         registerNib()
         tableView.dataSource = self
         tableView.delegate = self
+        decodeJSON()
+
     }
 
     @IBAction func tapAddButton(_ sender: Any) {
@@ -23,17 +26,33 @@ class ViewController: UIViewController {
     private func registerNib() {
         tableView.register(UINib(nibName: "DiaryTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
     }
+    
+    private func decodeJSON() {
+        let jsonDecoder = JSONDecoder()
+        guard let dataAsset = NSDataAsset(name: "sample") else { return }
+        
+        do {
+            self.sample = try jsonDecoder.decode([Sample].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        self.tableView.reloadData()
+    }
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.sample.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? DiaryTableViewCell else {
             return UITableViewCell()
         }
+        
+        let sample: Sample = self.sample[indexPath.row]
+        cell.configureLabel(sample: sample)
         
         return cell
     }
