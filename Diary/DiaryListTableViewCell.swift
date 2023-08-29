347b1b7bca72cb740d801cbc961defff33e6927b
hemg2 <hemg2@naver.com>
2023-08-29 11:15:30 +0900

    feat: tabelView 생성

---
+++ Diary/DiaryListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DiaryListTableViewCell.swift
+//  Diary
+//
+//  Created by 1 on 2023/08/29.
+//
+
+import UIKit
+
+class DiaryListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Diary/DiaryListViewController.swift
+++ Diary/DiaryListViewController.swift
@@ -7,10 +7,50 @@
 import UIKit
 
 class DiaryListViewController: UIViewController {
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    private var diaryEntity = [DiaryEntity]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+        setUpTableView()
+    }
+    
+    private func configureUI() {
         view.backgroundColor = .systemBackground
         self.title = "일기장"
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
+    private func setUpTableView() {
+        tableView.dataSource = self
+        tableView.dataSource = self
+        tableView.register(DiaryListTableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+extension DiaryListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        diaryEntity.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as?
+                DiaryListTableViewCell else { return UITableViewCell() }
+        
+        return cell
     }
 }
