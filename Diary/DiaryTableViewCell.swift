7da8507218c08ac4b68ab9caa22595fdead2e330
yyss99 <yyssjj9999@gmail.com>
2023-08-29 12:32:28 +0900

    feat: DiaryTableViewCell 생성 및 기본 구성

---
+++ Diary/DiaryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DiaryTableViewCell.swift
+//  Diary
+//
+//  Created by yyss99, Jusbug on 2023/08/29.
+//
+
+import UIKit
+
+class DiaryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
