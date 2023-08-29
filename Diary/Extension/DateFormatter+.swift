f948ca2abdfcad1ce1650a656d50e3f9d798165c
Kobe <mskang@xingxingmobility.com>
2023-08-29 17:59:27 +0900

    feat: 사용자 지역 포맷에 맞는 작성일자 반환을 위한 DateFormatter extension 구현 및 convertDate 함수 구현.

--- Diary/Extension/DateFormatter+.swift
+++ Diary/Extension/DateFormatter+.swift
@@ -2,7 +2,53 @@
 //  DateFormatter+.swift
 //  Diary
 //
-//  Created by Minseong Kang on 2023/08/29.
+//  Created by Kobe, Moon on 2023/08/29.
 //
 
 import Foundation
+
+extension DateFormatter {
+    func convertDate(_ date: Date, _ locale: String) -> String {
+        self.locale = Locale(identifier: locale)
+        let region = String(locale.suffix(2))
+        let localeID = Region(region: region)
+        let dateFormat = localeID.dateFormat
+        self.dateFormat = dateFormat
+        
+        return self.string(from: date)
+    }
+}
+extension DateFormatter {
+    enum Region: String {
+        case korea = "KR"
+        case usa = "US"
+        case japan = "JP"
+        case notSupported = "notSupported"
+        
+        init(region: String) {
+            switch region {
+            case "KR":
+                self = .korea
+            case "US":
+                self = .usa
+            case "JP":
+                self = .japan
+            default:
+                self = .notSupported
+            }
+        }
+        
+        var dateFormat: String {
+            switch self {
+            case .korea:
+                return "yyyy년 MM월 dd일"
+            case .usa:
+                return "MMM d, yyyy"
+            case .japan:
+                return "yyyy/MM/dd"
+            case .notSupported:
+                return "yyyy-MM-dd"
+            }
+        }
+    }
+}
