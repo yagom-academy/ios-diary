3ab40bdfb06aa9d6a28b33b897ea3657cb398567
YaRkyungmin <kkgm94@naver.com>
2023-08-30 11:29:03 +0900

    🩹chore: DataManager 컨벤션 수정

--- Diary/Model/DataManager.swift
+++ Diary/Model/DataManager.swift
@@ -10,8 +10,17 @@ import UIKit
 struct DataManager {
     func diary(diaryList: [DiaryData]) -> [Diary] {
         return diaryList.map {
-            let createdDate = DateFormatter().dateString(from: Date(timeIntervalSinceReferenceDate: Double($0.createdDate)), at: DateFormatter.Location.korea, with: DateFormatter.FormatTemplate.attached)
-            return Diary(identifier: UUID(), title: $0.title, body: $0.body, createdDate: createdDate)
+            let createdDate = DateFormatter().dateString(
+                from: Date(timeIntervalSince1970: Double($0.createdDate)),
+                at: DateFormatter.Location.korea,
+                with: DateFormatter.FormatTemplate.attached
+            )
+            return Diary(
+                identifier: UUID(),
+                title: $0.title,
+                body: $0.body,
+                createdDate: createdDate
+            )
         }
     }
 }
