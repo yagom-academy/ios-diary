4988d1d043249f8d618376ca72cbe8e3dd7d6212
JusBug <pksgwo123@gmail.com>
2023-08-30 11:28:14 +0900

    feat: TextView 생성 및 placeholder 구현

--- Diary/ViewController/DetailViewController.swift
+++ Diary/ViewController/DetailViewController.swift
@@ -8,8 +8,57 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet weak var titleTextView: UITextView!
+    @IBOutlet weak var bodyTextView: UITextView!
+    let placeHolderText = "Input Text"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initTitleTextView()
+        initbodyTextView()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.titleTextView.resignFirstResponder()
+    }
+    
+    private func initTitleTextView() {
+        titleTextView.text = placeHolderText
+        titleTextView.textColor = .lightGray
+        titleTextView.layer.borderWidth = 1
+        titleTextView.delegate = self
+    }
+    
+    private func initbodyTextView() {
+        bodyTextView.text = placeHolderText
+        bodyTextView.textColor = .lightGray
+        bodyTextView.layer.borderWidth = 1
+        bodyTextView.delegate = self
+    }
+}
+
+extension DetailViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if titleTextView.text == placeHolderText {
+            titleTextView.text = nil
+            titleTextView.textColor = .black
+        }
+        
+        if bodyTextView.text == placeHolderText {
+            bodyTextView.text = nil
+            bodyTextView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if titleTextView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            titleTextView.text = placeHolderText
+            titleTextView.textColor = .lightGray
+        }
+        
+        if bodyTextView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            bodyTextView.text = placeHolderText
+            bodyTextView.textColor = .lightGray
+        }
     }
 }
