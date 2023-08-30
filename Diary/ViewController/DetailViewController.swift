bca315035ab4805a72e0f8e5dcaee27c895f0d0f
yyss99 <yyssjj9999@gmail.com>
2023-08-30 15:39:33 +0900

    feat: DetailViewController 편집 시 키보드가 글자 가리는 이슈 해결

--- Diary/ViewController/DetailViewController.swift
+++ Diary/ViewController/DetailViewController.swift
@@ -11,6 +11,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var titleTextView: UITextView!
     @IBOutlet weak var bodyTextView: UITextView!
     private var sample: Sample?
+    private var originalFrame: CGRect?
     
     init?(sample: Sample, coder: NSCoder) {
         self.sample = sample
@@ -24,10 +25,40 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTextView()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+          self.view.endEditing(true)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     private func configureTextView() {
         titleTextView.text = sample?.title
         bodyTextView.text = sample?.body
+    }
+    
+    @objc func keyboardWillShow(_ sender: Notification) {
+        if originalFrame == nil {
+            originalFrame = view.frame
+        }
+        
+        if let keyboardFrame = (sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let keyboardHeight = keyboardFrame.height
+            let safeAreaBottom = view.safeAreaInsets.bottom
+            let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight - safeAreaBottom, right: 0)
+            
+            bodyTextView.contentInset = contentInsets
+        }
+    }
+    
+    @objc func keyboardWillHide(_ sender: Notification) {
+        if originalFrame != nil {
+            bodyTextView.contentInset = UIEdgeInsets.zero
+        }
     }
 }
