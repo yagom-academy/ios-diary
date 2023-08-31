ee217112079222c7ef54afa6f74cdbc0612704e6
MaryJo-github <jotjdal0215@gmail.com>
2023-08-31 20:33:57 +0900

    :sparkles: Feat: EditingDiaryViewController 생성

---
+++ Diary/Controller/EditingDiaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditingDiaryViewController.swift
+//  Diary
+//
+//  Created by MARY on 2023/08/30.
+//
+
+import UIKit
+
+class EditingDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
