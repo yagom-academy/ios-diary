7cff124803ce229387cb254c573645f81798cf39
h-suo <phstoo1021@naver.com>
2023-08-31 22:12:50 +0900

    ♻️Refactor: 데이터 로드에 실패했을때 Alert가 present 되도록 변경

--- Diary/Controller/DiaryListViewController.swift
+++ Diary/Controller/DiaryListViewController.swift
@@ -64,16 +64,24 @@ extension DiaryListViewController {
             dataSource.apply(snapshot)
         } catch {
             print(error.localizedDescription)
+            presentFailAlert()
         }
     }
 }
 
-// MARK: - Push ViewController
+// MARK: - Push & Present Controller
 extension DiaryListViewController {
     @objc private func pushDiaryViewController() {
         let diaryViewController = DiaryViewController(diaryStore: diaryStore, diaryEntry: nil)
         
         navigationController?.pushViewController(diaryViewController, animated: true)
+    }
+    
+    private func presentFailAlert() {
+        let alertAction = UIAlertAction(title: NameSpace.check, style: .default)
+        let alert = UIAlertController.customAlert(alertTile: NameSpace.fail, alertMessage: NameSpace.loadDataFail, preferredStyle: .alert, alertActions: [alertAction])
+        
+        self.present(alert, animated: true)
     }
 }
 
@@ -112,6 +120,7 @@ extension DiaryListViewController: UITableViewDelegate {
             navigationController?.pushViewController(diaryViewController, animated: true)
         } catch {
             print(error.localizedDescription)
+            presentFailAlert()
         }
     }
 }
@@ -157,5 +166,8 @@ extension DiaryListViewController {
     private enum NameSpace {
         static let diary = "일기장"
         static let plus = "plus"
+        static let fail = "실패"
+        static let loadDataFail = "데이터 로드에 실패했습니다."
+        static let check = "확인"
     }
 }
