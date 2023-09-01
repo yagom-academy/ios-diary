c51cdfe8ab6fee9e80fb346ebd545ef378c727ff
mint3382 <mskim0926@naver.com>
2023-09-01 23:42:27 +0900

    refactor: 불필요한 import 제거

--- Diary/App/AppDelegate.swift
+++ Diary/App/AppDelegate.swift
@@ -2,10 +2,9 @@
 //  Diary - AppDelegate.swift
 //  Created by yagom. 
 //  Copyright © yagom. All rights reserved.
-// 
+//
 
 import UIKit
-import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
