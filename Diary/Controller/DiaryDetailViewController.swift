f74392a76be4ff0ea3c28f25604af66b0b5137a2
YaRkyungmin <kkgm94@naver.com>
2023-09-01 15:27:11 +0900

    ✨feat: keyBoard 설정 추가
    ios 버전에 따른 notification 추가
    ios 버전에 따른 제약 수정

--- Diary/Controller/DiaryDetailViewController.swift
+++ Diary/Controller/DiaryDetailViewController.swift
@@ -30,7 +30,58 @@ final class DiaryDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureUI()
+        setupConstraint()
         setupComponents()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if #unavailable(iOS 15.0) {
+            addKeyboardObserver()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if #unavailable(iOS 15.0) {
+            removeKeyboardObserver()
+        }
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo as NSDictionary?,
+              let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+
+        diaryTextView.contentInset = UIEdgeInsets(top: .zero, left: .zero, bottom: keyboardFrame.size.height, right: .zero)
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        diaryTextView.contentInset = UIEdgeInsets.zero
+        diaryTextView.scrollIndicatorInsets = diaryTextView.contentInset
+    }
+}
+
+// MARK: Notification
+extension DiaryDetailViewController {
+    private func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    private func removeKeyboardObserver() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
 
@@ -48,6 +99,7 @@ extension DiaryDetailViewController {
     
     private func setupTextView() {
         diaryTextView.text = String(format: NameSpace.diaryText, diary.title, diary.body)
+        diaryTextView.keyboardDismissMode = .onDrag
     }
     
     private func setupNavigationBar() {
@@ -68,7 +120,29 @@ extension DiaryDetailViewController {
 
 // MARK: Setup Constraint
 extension DiaryDetailViewController {
-    private func setupConstraint() {}
+    private func setupConstraint() {
+        setupDiaryTextViewConstraint()
+    }
+    
+    private func setupDiaryTextViewConstraint() {
+        if #unavailable(iOS 15.0) {
+            NSLayoutConstraint.activate([
+                diaryTextView.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor),
+                diaryTextView.bottomAnchor.constraint(equalTo: view.readableContentGuide.bottomAnchor),
+                diaryTextView.leftAnchor.constraint(equalTo: view.readableContentGuide.leftAnchor),
+                diaryTextView.rightAnchor.constraint(equalTo: view.readableContentGuide.rightAnchor)
+            ])
+        } else {
+            NSLayoutConstraint.activate([
+                diaryTextView.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor),
+                diaryTextView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
+                diaryTextView.leftAnchor.constraint(equalTo: view.readableContentGuide.leftAnchor),
+                diaryTextView.rightAnchor.constraint(equalTo: view.readableContentGuide.rightAnchor)
+            ])
+        }
+    }
+}
+
 // MARK: Name Space
 extension DiaryDetailViewController {
     private enum NameSpace {
