767f5860379eae1261d75db73e41e7e3a5744715
mint3382 <mskim0926@naver.com>
2023-09-01 10:11:24 +0900

    refactor: UITextViewDelegate를 채택해 contentTextView 내부에서 제목 구분

--- Diary/ViewController/DiaryViewController.swift
+++ Diary/ViewController/DiaryViewController.swift
@@ -8,22 +8,43 @@
 import UIKit
 
 final class DiaryViewController: UIViewController {
+    private let diary: Diary?
+    var titleLine: Bool = false
     private let contentTextView: UITextView = {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
     }()
+    init(diary: Diary? = nil) {
+        self.diary = diary
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        contentTextView.delegate = self
         configureBackgroundColor()
         configureTextView()
+        fillTextView()
+        configureTextViewConstraint()
     }
     private func configureBackgroundColor() {
         view.backgroundColor = .systemBackground
     }
+    private func fillTextView() {
+        guard let diary else {
+            let dateFormatter = DateFormatter()
+            dateFormatter.configureDiaryDateFormat()
+            navigationItem.title = dateFormatter.string(from: Date())
+            return
+        }
+        contentTextView.text = diary.title + "\n\n" + diary.body
+        navigationItem.title = diary.date
+    }
     private func configureTextView() {
         view.addSubview(contentTextView)
-        configureTextViewConstraint()
     }
     private func configureTextViewConstraint() {
         NSLayoutConstraint.activate([
@@ -32,5 +53,14 @@ final class DiaryViewController: UIViewController {
             contentTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             contentTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+}
+extension DiaryViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if titleLine == false && text == "\n" {
+            contentTextView.text += "\n"
+            titleLine = true
+        }
+        return true
     }
 }
