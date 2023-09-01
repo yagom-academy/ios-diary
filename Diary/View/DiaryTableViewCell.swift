7efc69e1d044ca91a2030818d6cd2615d3eb47e3
mint3382 <mskim0926@naver.com>
2023-09-02 00:07:36 +0900

    refactor: 불필요한 코드 제거

--- Diary/View/DiaryTableViewCell.swift
+++ Diary/View/DiaryTableViewCell.swift
@@ -51,10 +51,6 @@ final class DiaryTableViewCell: UITableViewCell {
         return label
     }()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
     required override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         configureCellSubviews()
@@ -63,10 +59,6 @@ final class DiaryTableViewCell: UITableViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
     }
     
     func configureContents(diary: Diary) {
