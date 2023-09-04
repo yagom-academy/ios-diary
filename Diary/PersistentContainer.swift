bda228c76fe91a08e78b1cffb304f96de30e1073
mint3382 <mskim0926@naver.com>
2023-09-04 22:41:40 +0900

    feat: PersistentContainer class 생성

---
+++ Diary/PersistentContainer.swift
@@ -0,0 +1,20 @@
+//
+//  PersistentContainer.swift
+//  Diary
+//
+//  Created by MINT, BMO on 2023/09/04.
+//
+
+import CoreData
+
+class PersistentContainer: NSPersistentContainer {
+    func saveContext(backgroundContext: NSManagedObjectContext? = nil) {
+        let context = backgroundContext ?? viewContext
+        guard context.hasChanges else { return }
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print("Error: \(error), \(error.userInfo)")
+        }
+    }
+}
