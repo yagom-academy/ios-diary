1be2d589ee1da9fcd1f5b0a81ad84c5150adb766
mint3382 <mskim0926@naver.com>
2023-09-04 22:40:55 +0900

    feat: CoreData Entity 및 클래스 생성

---
+++ DiaryEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryEntity+CoreDataClass.swift
+//  Diary
+//
+//  Created by MINT, BMO on 2023/09/04.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DiaryEntity)
+public class DiaryEntity: NSManagedObject {
+
+}
---
+++ DiaryEntity+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  DiaryEntity+CoreDataProperties.swift
+//  Diary
+//
+//  Created by MINT, BMO on 2023/09/04.
+//
+//
+
+import Foundation
+import CoreData
+
+extension DiaryEntity {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<DiaryEntity> {
+        return NSFetchRequest<DiaryEntity>(entityName: "DiaryEntity")
+    }
+
+    @NSManaged public var body: String?
+    @NSManaged public var date: Date?
+    @NSManaged public var title: String?
+
+}
+
+extension DiaryEntity: Identifiable {
+
+}
