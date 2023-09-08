0b970e8c557f0421287af2d89345a42c2be7bca3
mint3382 <mskim0926@naver.com>
2023-09-08 15:05:45 +0900

    feat: DiaryAlertPresentable protocol 구현

---
+++ Diary/Protocol/DiaryAlertPresentable.swift
@@ -0,0 +1,27 @@
+//
+//  DiaryAlertPresentable.swift
+//  Diary
+//
+//  Created by MINT, BMO on 2023/09/08.
+//
+
+import UIKit
+
+protocol DiaryAlertPresentable {
+    func showDeleteConfirmAlert(in viewController: UIViewController, by action: @escaping () -> Void)
+}
+
+extension DiaryAlertPresentable {
+    func showDeleteConfirmAlert(in viewController: UIViewController, by action: @escaping () -> Void) {
+        let alert = UIAlertController(title: "진짜요?", message: "정말로 삭제하시겠어요?", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+        let deleteAction = UIAlertAction(title: "삭제", style: .destructive) { _ in
+            action()
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(deleteAction)
+        
+        viewController.present(alert, animated: true)
+    }
+}
