f23e7cb8a0cc1cc5d8ac51e51643009c975f1212
h-suo <phstoo1021@naver.com>
2023-09-08 15:34:50 +0900

    ✨Feat: CRUD 메서드 구현

--- Diary/Model/Storage/CoreDataDiaryStorage.swift
+++ Diary/Model/Storage/CoreDataDiaryStorage.swift
@@ -40,33 +40,80 @@ final class CoreDataDiaryStorage: DiaryStorageProtocol {
             }
         }
     }
+    
+    // MARK: - Core Data CRUD
     func diaryEntrys() throws -> [DiaryEntry] {
-        guard let context,
-              let diaryEntitys = try context.fetch(DiaryEntity.fetchRequest()) as? [DiaryEntity] else {
+        if let diaryEntitys = try context.fetch(DiaryEntity.fetchRequest()) as? [DiaryEntity] {
+            let diaryEntrys = try diaryEntitys.map {
+                try $0.diaryEntry()
+            }
+            
+            return diaryEntrys
+        } else {
             throw StorageError.loadDataFailed
         }
-        
-        let diaryEntrys = try diaryEntitys.map {
-            try $0.diaryEntry()
-        }
-        
-        return diaryEntrys
     }
     
     func storeDiary(_ diary: DiaryEntry) throws {
-        guard let context,
-              let entity = NSEntityDescription.entity(forEntityName: "DiaryEntity", in: context),
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: NameSpace.diaryEntity, in: context),
               let creationDate = DateFormatManager.timestamp(localeDateFormatter: UserDateFormatter.shared, string: diary.creationDate) else {
-            throw StorageError.saveDataFailed
+            throw StorageError.storeDataFailed
         }
         
-        let diaryEntity = NSManagedObject(entity: entity, insertInto: context)
-        diaryEntity.setValue(diary.id, forKey: "id")
-        diaryEntity.setValue(diary.title, forKey: "title")
-        diaryEntity.setValue(diary.body, forKey: "body")
-        diaryEntity.setValue(creationDate, forKey: "creationDate")
+        let diaryEntity = NSManagedObject(entity: entityDescription, insertInto: context)
+        diaryEntity.setValue(diary.id, forKey: NameSpace.id)
+        diaryEntity.setValue(diary.title, forKey: NameSpace.title)
+        diaryEntity.setValue(diary.body, forKey: NameSpace.body)
+        diaryEntity.setValue(creationDate, forKey: NameSpace.creationDate)
         
         try context.save()
     }
     
+    func updateDiary(_ diary: DiaryEntry) throws {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>.init(entityName: NameSpace.diaryEntity)
+        fetchRequest.predicate = NSPredicate(format: NameSpace.idEqualFormat, diary.id.uuidString)
+        
+        if let diaryEntity = try context.fetch(fetchRequest).first as? NSManagedObject,
+           let creationDate = DateFormatManager.timestamp(localeDateFormatter: UserDateFormatter.shared, string: diary.creationDate) {
+            
+            diaryEntity.setValue(diary.title, forKey: NameSpace.title)
+            diaryEntity.setValue(diary.body, forKey: NameSpace.body)
+            diaryEntity.setValue(creationDate, forKey: NameSpace.creationDate)
+            
+            try context.save()
+        } else {
+            throw StorageError.updateDataFailed
+        }
+    }
+    
+    func deleteDiary(_ diary: DiaryEntry) throws {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>.init(entityName: NameSpace.diaryEntity)
+        fetchRequest.predicate = NSPredicate(format: NameSpace.idEqualFormat, diary.id.uuidString)
+        
+        if let diaryEntity = try context.fetch(fetchRequest).first as? NSManagedObject {
+            context.delete(diaryEntity)
+            try context.save()
+        } else {
+            throw StorageError.deleteDataFailed
+        }
+    }
+    
+    func deleteAll() throws {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>.init(entityName: NameSpace.diaryEntity)
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        
+        try context.execute(deleteRequest)
+    }
+}
+
+extension CoreDataDiaryStorage {
+    private enum NameSpace {
+        static let diary = "Diary"
+        static let diaryEntity = "DiaryEntity"
+        static let id = "id"
+        static let title = "title"
+        static let body = "body"
+        static let creationDate = "creationDate"
+        static let idEqualFormat = "id == %@"
+    }
 }
