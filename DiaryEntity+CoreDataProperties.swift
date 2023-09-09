f23d942892a9180186ae2c75b957ed72cad585bf
h-suo <phstoo1021@naver.com>
2023-09-09 10:28:22 +0900

    ♻️Refactor: DiaryEntity 프로퍼티 수정

--- DiaryEntity+CoreDataProperties.swift
+++ DiaryEntity+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  DiaryEntity+CoreDataProperties.swift
 //  Diary
 //
-//  Created by Erick on 2023/09/04.
+//  Created by Erick on 2023/09/08.
 //
 //
 
@@ -10,30 +10,24 @@ import Foundation
 import CoreData
 
 extension DiaryEntity {
-    
+
     @nonobjc public class func fetchRequest() -> NSFetchRequest<DiaryEntity> {
         return NSFetchRequest<DiaryEntity>(entityName: "DiaryEntity")
     }
-    
-    @NSManaged public var title: String?
+
+    @NSManaged public var id: UUID
+    @NSManaged public var title: String
     @NSManaged public var body: String?
-    @NSManaged public var creationDate: Int32
-    @NSManaged public var id: UUID?
-    
+    @NSManaged public var creationDate: Date
 }
 
 extension DiaryEntity: Identifiable {}
 
 extension DiaryEntity {
     func diaryEntry() throws -> DiaryEntry {
-        let creationDate = DateFormatManager.string(localeDateFormatter: UserDateFormatter.shared, timestamp: Int(creationDate))
+        let timeStamp = DateFormatManager.timestamp(date: creationDate)
+        let dateFormatString = DateFormatManager.string(localeDateFormatter: UserDateFormatter.shared, timestamp: timeStamp)
         
-        guard let title,
-              let body,
-              let id else {
-            throw ConversionError.missingAttribute
-        }
-        
-        return DiaryEntry(id: id, title: title, body: body, creationDate: creationDate)
+        return DiaryEntry(id: id, title: title, body: body ?? "", creationDate: dateFormatString)
     }
 }
