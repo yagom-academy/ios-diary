edce1c97989d442b371a36df669507f10e220142
h-suo <phstoo1021@naver.com>
2023-09-10 12:32:22 +0900

    ♻️Refactor: 사용하지 않는 에러 케이스 삭제

--- Diary/Error/StorageError.swift
+++ Diary/Error/StorageError.swift
@@ -9,20 +9,11 @@ import Foundation
 
 enum StorageError: LocalizedError {
     case loadDataFailed
-    case storeDataFailed
-    case updateDataFailed
-    case deleteDataFailed
     
     public var errorDescription: String? {
         switch self {
         case .loadDataFailed:
             return "데이터 로드를 실패했습니다."
-        case .storeDataFailed:
-            return "데이터 저장을 실패했습니다."
-        case .updateDataFailed:
-            return "데이터 업데이트를 실패했습니다."
-        case .deleteDataFailed:
-            return "데이터 삭제를 실패했습니다."
         }
     }
 }
