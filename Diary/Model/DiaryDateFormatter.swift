198024c8a6a9fdeee2995f2133cb60f73241be0e
Kobe <mskang@xingxingmobility.com>
2023-09-10 12:12:02 +0900

    refactor: 사용하지 않는 fetchDate(_:,_:) 함수 삭제.

--- Diary/Model/DiaryDateFormatter.swift
+++ Diary/Model/DiaryDateFormatter.swift
@@ -21,13 +21,6 @@ final class DiaryDateFormatter {
         
         return formatter.string(from: date)
     }
-    
-    static func fetchDate(_ createdAt: Int, _ locale: String) -> String {
-        let timeInterval = TimeInterval(createdAt)
-        let date = Date(timeIntervalSince1970: timeInterval)
-        
-        return self.convertDate(date, locale)
-    }
 }
 
 extension DiaryDateFormatter {
