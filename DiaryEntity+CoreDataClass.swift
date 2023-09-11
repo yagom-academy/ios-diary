5b515b6bb7e1cd47157218b6db71d9b7711ca83f
DasanKim <tempzipp@gmail.com>
2023-09-11 19:50:38 +0900

    ✨feat: Core Data DiaryEntity 생성

---
+++ DiaryEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryEntity+CoreDataClass.swift
+//  Diary
+//
+//  Created by Yena on 2023/09/11.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DiaryEntity)
+public class DiaryEntity: NSManagedObject {
+
+}
