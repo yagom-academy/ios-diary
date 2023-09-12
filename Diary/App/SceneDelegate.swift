ff2f30d9191c492ced434268d3b05395cb02a402
YaRkyungmin <kkgm94@naver.com>
2023-09-12 16:46:26 +0900

    🩹chore: 불필요한 코드 및 주석 삭제

--- Diary/App/SceneDelegate.swift
+++ Diary/App/SceneDelegate.swift
@@ -31,8 +31,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneWillEnterForeground(_ scene: UIScene) {}
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
-    }
+    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
 
