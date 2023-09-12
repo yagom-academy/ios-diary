f71681b54aaf426d01d3245e23d129773bea595e
h-suo <phstoo1021@naver.com>
2023-09-12 13:25:55 +0900

    ✅Test: 테스트 코드 수정

--- DiaryTests/AssetDiaryReaderTests.swift
+++ DiaryTests/AssetDiaryReaderTests.swift
@@ -1,5 +1,5 @@
 //
-//  AssetDiaryStorageTests.swift
+//  AssetDiaryReaderTests.swift
 //  DiaryStorageTests
 //
 //  Created by Erick on 2023/08/30.
@@ -8,18 +8,18 @@
 import XCTest
 @testable import Diary
 
-final class AssetDiaryStorageTests: XCTestCase {
-    var sut: AssetDiaryStorage?
+final class AssetDiaryReaderTests: XCTestCase {
+    var sut: AssetDiaryReader?
     
     override func setUpWithError() throws {
-        sut = AssetDiaryStorage()
+        sut = AssetDiaryReader()
     }
 
     override func tearDownWithError() throws {
         sut = nil
     }
 
-    func test_AssetDiaryStorage의_DiaryEntrys를_사용하면_DiaryEntry배열을_반환합니다() {
+    func test_AssetDiaryReader의_DiaryEntrys를_사용하면_DiaryEntry배열을_반환합니다() {
         do {
             // when
             let result = try sut?.diaryEntrys() is [DiaryEntry]
--- DiaryTests/CoreDataDiaryManagerTests.swift
+++ DiaryTests/CoreDataDiaryManagerTests.swift
@@ -1,5 +1,5 @@
 //
-//  CoreDataDiaryStorage.swift
+//  CoreDataDiaryManagerTests.swift
 //  DiaryTests
 //
 //  Created by Erick on 2023/09/08.
@@ -8,18 +8,18 @@
 import XCTest
 @testable import Diary
 
-final class CoreDataDiaryStorageTests: XCTestCase {
-    var sut: CoreDataDiaryStorage!
+final class CoreDataDiaryManagerTests: XCTestCase {
+    var sut: CoreDataDiaryManager!
     
     override func setUpWithError() throws {
-        sut = CoreDataDiaryStorage()
+        sut = CoreDataDiaryManager()
     }
 
     override func tearDownWithError() throws {
         try sut.deleteAll()
     }
 
-    func test_CoreDataDiaryStorage의_DiaryEntrys를_사용하면_DiaryEntry배열을_반환합니다() {
+    func test_CoreDataDiaryManager의_DiaryEntrys를_사용하면_DiaryEntry배열을_반환합니다() {
         do {
             // when
             let result = try sut.diaryEntrys() is [DiaryEntry]
@@ -33,7 +33,7 @@ final class CoreDataDiaryStorageTests: XCTestCase {
     func test_storeDiary로_일기를_저장할_수_있습니다() {
         do {
             // given
-            sut.storeDiary(title: "test", body: "store test")
+            try sut.storeDiary(title: "test", body: "store test")
             let expectation = "store test"
             // when
             let result = try sut.diaryEntrys().first?.body
@@ -47,11 +47,11 @@ final class CoreDataDiaryStorageTests: XCTestCase {
     func test_updateDiary로_저장된_일기를_수정할_수_있습니다() {
         do {
             // given
-            sut.storeDiary(title: "test", body: "update test")
+            try sut.storeDiary(title: "test", body: "update test")
             var diaryEntry = try sut.diaryEntrys().first
             diaryEntry?.body = "update test update test"
             // when
-            sut.updateDiary(diaryEntry!)
+            try sut.updateDiary(diaryEntry!)
             let result = try sut.diaryEntrys().first
             // then
             XCTAssertEqual(result, diaryEntry)
@@ -63,10 +63,10 @@ final class CoreDataDiaryStorageTests: XCTestCase {
     func test_deleteDiary로_원하는_일기를_삭제할_수_있습니다() {
         do {
             // given
-            sut.storeDiary(title: "test", body: "delete test")
+            try sut.storeDiary(title: "test", body: "delete test")
             let diaryEntry = try sut.diaryEntrys().first
             // when
-            sut.deleteDiary(diaryEntry!)
+            try sut.deleteDiary(diaryEntry!)
             let result = try sut.diaryEntrys().isEmpty
             // then
             XCTAssertTrue(result)
