e343195b253db0733d0991ba9d533d18d182252a
Toy <woohyeon3382@naver.com>
2024-01-02 15:28:14 +0900

    feat: SampleData 타입 생성 및 CodingKeys 구현

---
+++ Diary/SampleData.swift
@@ -0,0 +1,20 @@
+//
+//  SampleData.swift
+//  Diary
+//
+//  Created by Toy, Morgan on 1/2/24.
+//
+
+import Foundation
+
+struct SampleData: Decodable {
+    let title: String
+    let body: String
+    let createdAt: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case body
+        case createdAt = "created_at"
+    }
+}
