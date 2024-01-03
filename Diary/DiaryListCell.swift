b912a6194e3492cc700c12aad2ae1f66c169e63f
Morgan <devjoon@naver.com>
2024-01-03 17:05:39 +0900

    feat: DiaryListCell 기본구현

--- Diary/DiaryListCell.swift
+++ Diary/DiaryListCell.swift
@@ -11,6 +11,60 @@ class DiaryListCell: UITableViewCell {
 
     static let identifier = "DiaryListCell"
     
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.spacing = 2
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let previewLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    //MARK: - LifeCycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Helper
+    private func setupLabel() {
+//        contentView.addSubview(titleLabel)
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+    
+    private func setupStackView() {
+        stackView.addSubview(dateLabel)
+        stackView.addSubview(previewLabel)
+        
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
