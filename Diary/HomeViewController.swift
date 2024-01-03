986fced8d7c5cd5681883a47aaae99d4c9a50262
Hisop <khsdfg958@gmail.com>
2024-01-03 14:41:12 +0900

    feat: initFunction 추가

--- Diary/HomeViewController.swift
+++ Diary/HomeViewController.swift
@@ -6,13 +6,49 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UITableViewDataSource {
+    private let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
         // Do any additional setup after loading the view.
     }
+    
 
+}
 
+extension HomeViewController {
+    private func configureUI() {
+        navigationItemInit()
+        tableInit()
+        autoLayoutInit()
+    }
+    
+    private func navigationItemInit() {
+        let rightItemButton = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: nil, action: nil)
+        
+        self.navigationItem.title = "일기장"
+        self.navigationItem.rightBarButtonItem = rightItemButton
+    }
+    
+    private func tableInit() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    private func autoLayoutInit() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
