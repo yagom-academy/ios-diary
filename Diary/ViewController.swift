3b8a67b8c19ac3aa5b47261087ce4eb351ba0c52
Morgan <devjoon@naver.com>
2024-01-03 16:18:48 +0900

    feat: TableView 생성

--- Diary/ViewController.swift
+++ Diary/ViewController.swift
@@ -7,17 +7,23 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    //MARK: - property
+    private let tableView = UITableView()
+    
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupNavigationBar()
+        setupTableView()
     }
     
+    //MARK: - Helper
     private func setupNavigationBar() {
         let apearance = UINavigationBarAppearance()
         apearance.configureWithOpaqueBackground()
-        apearance.backgroundColor = .white
+        apearance.backgroundColor = .red
         
         navigationItem.title = "일기장"
         navigationController?.navigationBar.standardAppearance = apearance
@@ -25,5 +31,15 @@ final class ViewController: UIViewController {
         navigationController?.navigationBar.scrollEdgeAppearance = apearance
     }
     
-    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor)
+        ])
+    }
 }
